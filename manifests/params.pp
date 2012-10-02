theforeman/puppet-foreman_proxy

commit c64274554ea54a0a2adb85561e949228731dd5a5

puppet-lint fixes

manifests/params.pp
@@ -60,7 +60,7 @@ class foreman_proxy::params {
       $dhcp_leases = '/var/lib/dhcpd/dhcpd.leases'
     }
   }
- 
+
   # DNS settings - requires optional DNS puppet module
   $dns           = false
   $dns_interface = 'eth0'

manifests/proxydhcp.pp
@@ -1,33 +1,31 @@
 class foreman_proxy::proxydhcp {
   $ip_temp   = "ipaddress_${foreman_proxy::dhcp_interface}"
-  $ip        = inline_template("<%= scope.lookupvar(ip_temp) %>")
+  $ip        = inline_template('<%= scope.lookupvar(ip_temp) %>')
 
   $net_temp  = "::network_${foreman_proxy::dhcp_interface}"
-  $net       = inline_template("<%= scope.lookupvar(net_temp) %>")
+  $net       = inline_template('<%= scope.lookupvar(net_temp) %>')
 
   $mask_temp = "::netmask_${foreman_proxy::dhcp_interface}"
-  $mask      = inline_template("<%= scope.lookupvar(mask_temp) %>")
+  $mask      = inline_template('<%= scope.lookupvar(mask_temp) %>')
 
   class { 'dhcp':
     dnsdomain    => [
-      "${::domain}",
-      "${foreman_proxy::dhcp_reverse}",
+      $::domain,
+      $foreman_proxy::dhcp_reverse,
     ],
-    nameservers  => ["${ip}"],
+    nameservers  => [$::ip],
     ntpservers   => ['us.pool.ntp.org'],
-    interfaces   => ["${foreman_proxy::dhcp_interface}"],
-    #dnsupdatekey => "/etc/bind/keys.d/foreman",
+    interfaces   => [$foreman_proxy::dhcp_interface],
+    #dnsupdatekey => /etc/bind/keys.d/foreman,
     #require      => Bind::Key[ 'foreman' ],
-    pxeserver    => "${ip}",
+    pxeserver    => $::ip,
     pxefilename  => 'pxelinux.0',
   }
 
-  dhcp::pool{ "${::domain}":
-    network => "${net}",
-    mask    => "${mask}",
-    range   => "${foreman_proxy::range}",
-    gateway => "${foreman_proxy::gateway}",
+  dhcp::pool{ $::domain:
+    network => $net,
+    mask    => $mask,
+    range   => $foreman_proxy::range,
+    gateway => $foreman_proxy::gateway,
   }
-
-
 }

manifests/config.pp
@@ -8,7 +8,7 @@ class foreman_proxy::config {
     require => Class['foreman_proxy::install'],
     notify  => Class['foreman_proxy::service'],
   }
- 
+
   file{'/etc/foreman-proxy/settings.yml':
     content => template('foreman_proxy/settings.yml.erb'),
     owner   => $foreman_proxy::user,

manifests/install.pp
@@ -1,6 +1,6 @@
 class foreman_proxy::install {
   class { '::foreman::install::repos':
-    use_testing => $foreman_proxy::use_testing,
+    use_testing    => $foreman_proxy::use_testing,
     package_source => $foreman_proxy::package_source,
   }
 

manifests/proxydns.pp
@@ -2,17 +2,17 @@ class foreman_proxy::proxydns {
   include dns
 
   $ip_temp = "::ipaddress_${foreman_proxy::dns_interface}"
-  $ip      = inline_template("<%= scope.lookupvar(ip_temp) %>")
+  $ip      = inline_template('<%= scope.lookupvar(ip_temp) %>')
 
-  dns::zone { "${::domain}":
-    soa     => "${::fqdn}",
-    reverse => "false",
-    soaip   => "${ip}",
+  dns::zone { $::domain:
+    soa     => $::fqdn,
+    reverse => false,
+    soaip   => $ip,
   }
 
-  dns::zone { "${foreman_proxy::dns_reverse}":
-    soa     => "${::fqdn}",
-    reverse => "true",
-    soaip   => "${ip}",
+  dns::zone { $foreman_proxy::dns_reverse:
+    soa     => $::fqdn,
+    reverse => true,
+    soaip   => $ip,
   }
 }

commit cb908b9a219a7a310d052a01d8114b7e50b01501

- Clean up OS specific DHCP settings - Fix a regression which breaks the DHCP config   I introduced this in c64274554ea54a0a2adb85561e949228731dd5a5

manifests/params.pp
@@ -43,16 +43,22 @@ class foreman_proxy::params {
   $dhcp_reverse   = '100.168.192.in-addr.arpa'
   $gateway        = '192.168.100.1'
   $range          = '192.168.100.50 192.168.100.200'
+
+  # DHCP server settings
   case $::operatingsystem {
-    Debian: {
+    Debian,Ubuntu: {
       $dhcp_vendor = 'isc'
       $dhcp_config = '/etc/dhcp/dhcpd.conf'
       $dhcp_leases = '/var/lib/dhcp/dhcpd.leases'
     }
-    Ubuntu: {
+    RedHat,CentOS: {
       $dhcp_vendor = 'isc'
-      $dhcp_config = '/etc/dhcp/dhcpd.conf'
-      $dhcp_leases = '/var/lib/dhcp/dhcpd.leases'
+      if ($::lsbmajdistrelease == 5) {
+        $dhcp_config = '/etc/dhcpd.conf'
+      } else {
+        $dhcp_config = '/etc/dhcp/dhcpd.conf'
+      }
+      $dhcp_leases = '/var/lib/dhcpd/dhcpd.leases'
     }
     default: {
       $dhcp_vendor = 'isc'

manifests/proxydhcp.pp
@@ -13,12 +13,12 @@ class foreman_proxy::proxydhcp {
       $::domain,
       $foreman_proxy::dhcp_reverse,
     ],
-    nameservers  => [$::ip],
+    nameservers  => [$ip],
     ntpservers   => ['us.pool.ntp.org'],
     interfaces   => [$foreman_proxy::dhcp_interface],
     #dnsupdatekey => /etc/bind/keys.d/foreman,
     #require      => Bind::Key[ 'foreman' ],
-    pxeserver    => $::ip,
+    pxeserver    => $ip,
     pxefilename  => 'pxelinux.0',
   }
 