b2f4159b4200e92254cad8ac9efebd7492f5e147
Justin Sherrill <jsherril@redhat.com>
2014-08-12 12:39:17 -0400

    fixes #7052 - adding pulpnode plugin support

--- manifests/plugin/pulp/params.pp
+++ manifests/plugin/pulp/params.pp
@@ -1,6 +1,7 @@
 # Default parameters for the Pulp smart proxy plugin
 class foreman_proxy::plugin::pulp::params {
-  $enabled  = true
-  $group    = undef
-  $pulp_url = "https://${::fqdn}/pulp"
+  $enabled          = true
+  $group            = undef
+  $pulpnode_enabled = false
+  $pulp_url         = "https://${::fqdn}/pulp"
 }
