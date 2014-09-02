a45ff2013e1c614c862f538f50a073a0222af550
Dominic Cleal <dcleal@redhat.com>
2014-09-02 15:38:13 +0100

    Syncing module configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+# This file is managed centrally by modulesync
+#   https://github.com/theforeman/foreman-installer-modulesync
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+# Workaround for no method in rspec-puppet to pass undef through :params
+class Undef
+  def inspect; 'undef'; end
+end
