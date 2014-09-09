04b95ba60361e478dfcaf4b08bdc9aa6d72871be
Dominic Cleal <dcleal@redhat.com>
2014-09-09 15:33:18 +0100

    Fix error referencing class that may not have been evaluated

--- manifests/plugin/pulp/params.pp
+++ manifests/plugin/pulp/params.pp
@@ -1,6 +1,6 @@
 # Default parameters for the Pulp smart proxy plugin
 class foreman_proxy::plugin::pulp::params {
   $enabled  = true
-  $group     = $::foreman_proxy::user
+  $group    = undef
   $pulp_url = "https://${::fqdn}/pulp"
 }
