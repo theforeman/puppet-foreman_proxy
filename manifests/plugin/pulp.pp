0910a12516d7393ab7995b810473331f7a3f0e8a
Dominic Cleal <dcleal@redhat.com>
2014-09-09 15:33:18 +0100

    Fix error referencing class that may not have been evaluated
    Closes GH-121

--- manifests/plugin/pulp.pp
+++ manifests/plugin/pulp.pp
@@ -11,10 +11,12 @@
 # $pulp_url::     pulp url to use
 #
 class foreman_proxy::plugin::pulp (
-  $enabled  = $foreman_proxy::plugin::pulp::params::enabled,
-  $group = $foreman_proxy::plugin::pulp::params::group,
-  $pulp_url = $foreman_proxy::plugin::pulp::params::pulp_url,
+  $enabled  = $::foreman_proxy::plugin::pulp::params::enabled,
+  $group    = $::foreman_proxy::plugin::pulp::params::group,
+  $pulp_url = $::foreman_proxy::plugin::pulp::params::pulp_url,
 ) inherits foreman_proxy::plugin::pulp::params {
+  $group_real = pick($group, $::foreman_proxy::user)
+  validate_string($group_real)
 
   foreman_proxy::plugin {'pulp':
   } ->
@@ -22,7 +24,7 @@ class foreman_proxy::plugin::pulp (
     ensure  => file,
     content => template('foreman_proxy/plugin/pulp.yml.erb'),
     owner   => 'root',
-    group   => $group,
+    group   => $group_real,
     mode    => '0640',
   }
 }
---
+++ spec/classes/foreman_proxy__plugin__pulp_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe 'foreman_proxy::plugin::pulp' do
+
+  let :facts do {
+    :osfamily               => 'RedHat',
+    :operatingsystem        => 'CentOS',
+    :operatingsystemrelease => '6.5',
+    :fqdn                   => 'my.host.example.com',
+  } end
+
+  describe 'with default settings' do
+    let :pre_condition do
+      "include foreman_proxy"
+    end
+
+    it { should contain_foreman_proxy__plugin('pulp') }
+    it 'should configure pulp.yml' do
+      should contain_file('/etc/foreman-proxy/settings.d/pulp.yml').
+        with({
+          :ensure  => 'file',
+          :owner   => 'root',
+          :group   => 'foreman-proxy',
+          :mode    => '0640',
+          :content => /:enabled: true/
+        })
+    end
+  end
+
+  describe 'with group overridden' do
+    let :pre_condition do
+      "include foreman_proxy"
+    end
+    let :params do {
+      :group => 'example',
+    } end
+
+    it 'should change pulp.yml group' do
+      should contain_file('/etc/foreman-proxy/settings.d/pulp.yml').
+        with({
+          :owner   => 'root',
+          :group   => 'example'
+        })
+    end
+  end
+end
