b1b815e889bc031ed3583fa391f78d5b363cb0c1
Cristian Falcas <falcas@optymyze.com>
2015-01-07 16:37:52 +0200

    add abrt plugin
    Fixes GH-133

---
+++ manifests/plugin/abrt/params.pp
@@ -0,0 +1,13 @@
+# Default parameters for the Pulp smart proxy plugin
+class foreman_proxy::plugin::abrt::params {
+  $enabled                 = true
+  $group                   = undef
+  $abrt_send_log_file      = '/var/log/foreman-proxy/abrt-send.log'
+  $spooldir                = '/var/spool/foreman-proxy-abrt'
+  $aggregate_reports       = true
+  $send_period             = 600
+  $faf_server_url          = undef
+  $faf_server_ssl_noverify = true
+  $faf_server_ssl_cert     = ''
+  $faf_server_ssl_key      = ''
+}
