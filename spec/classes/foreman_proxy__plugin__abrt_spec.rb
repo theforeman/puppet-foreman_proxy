b5940fa8f0bcd85264538432367d85ffd6ae3afc
Cristian Falcas <falcas@optymyze.com>
2015-01-22 16:35:34 +0200

    add abrt tests
    Closes GH-142

---
+++ spec/classes/foreman_proxy__plugin__abrt_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe 'foreman_proxy::plugin::abrt' do
+
+  let :facts do {
+    :osfamily               => 'RedHat',
+    :operatingsystem        => 'CentOS',
+    :operatingsystemrelease => '6.5',
+    :fqdn                   => 'my.host.example.com',
+  } end
+
+  describe 'with default settings' do
+    let :pre_condition do
+      "include foreman_proxy"
+    end
+
+    it { should contain_foreman_proxy__plugin('abrt') }
+    it 'should configure abrt.yml' do
+      should contain_file('/etc/foreman-proxy/settings.d/abrt.yml').
+        with({
+          :ensure  => 'file',
+          :owner   => 'root',
+          :group   => 'foreman-proxy',
+          :mode    => '0640',
+          :content => /:enabled: true/
+        })
+    end
+  end
+
+  describe 'with group overridden' do
+    let :pre_condition do
+      "include foreman_proxy"
+    end
+    let :params do {
+      :group => 'example',
+    } end
+
+    it 'should change abrt.yml group' do
+      should contain_file('/etc/foreman-proxy/settings.d/abrt.yml').
+        with({
+          :owner   => 'root',
+          :group   => 'example'
+        })
+    end
+  end
+end
