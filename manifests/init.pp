b3ebfb236958821d09ac2a850e4bfe03c288f652
Callum Scott <callum.scott@zonal.co.uk>
2015-02-17 13:47:31 +0000

    update comment to reflect that option_domain is now an array

--- manifests/init.pp
+++ manifests/init.pp
@@ -152,8 +152,8 @@
 #
 # $dhcp_managed::               DHCP is managed by Foreman proxy
 #                               type:boolean
-# $dhcp_use_option_domain::     DHCP use the dhcpd config option domain-name
-#                               type:boolean
+# $dhcp_option_domain::     DHCP use the dhcpd config option domain-name
+#                               type:array
 #
 # $dhcp_interface::             DHCP listen interface
 #
