a41549f3e2bf69f8f67c63ec09784f03857306ab
Dominic Cleal <dcleal@redhat.com>
2015-03-11 17:05:16 +0000

    Replace private() with comment for future parser compatibility

--- manifests/tftp/copy_file.pp
+++ manifests/tftp/copy_file.pp
@@ -1,9 +1,10 @@
 # Copy a TFTP file
+#
+# Private defined type, not for external use.
 define foreman_proxy::tftp::copy_file(
   $target_path,
   $source_file = $title,
 ) {
-  private()
   $filename = inline_template('<%= File.basename(@source_file) %>')
   file {"${target_path}/${filename}":
     ensure => file,
