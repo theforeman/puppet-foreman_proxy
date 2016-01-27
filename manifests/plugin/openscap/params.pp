c026825262947d356c5f0da9533bc6767a188769
Cristian Falcas <cristi.falcas@gmail.com>
2016-01-27 13:54:35 +0200

    add version to the openscap plugin
    closes GH-215

--- manifests/plugin/openscap/params.pp
+++ manifests/plugin/openscap/params.pp
@@ -1,6 +1,7 @@
 # Default parameters for the OpenSCAP smart proxy plugin
 class foreman_proxy::plugin::openscap::params {
   $enabled                 = true
+  $version                 = undef
   $configure_openscap_repo = false
   $listen_on               = 'https'
   $openscap_send_log_file  = '/var/log/foreman-proxy/openscap-send.log'
