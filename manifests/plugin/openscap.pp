dab44c21b0919b40bc1a80b8d8eb2eb292373a9a
Cristian Falcas <cristi.falcas@gmail.com>
2016-01-27 13:54:35 +0200

    add version to the openscap plugin

--- manifests/plugin/openscap.pp
+++ manifests/plugin/openscap.pp
@@ -4,6 +4,9 @@
 #
 # === Parameters:
 #
+# $version::                    plugin package version, it's passed to ensure parameter of package resource
+#                               can be set to specific version number, 'latest', 'present' etc.
+#
 # $enabled::                    enables/disables the plugin
 #                               type:boolean
 #
@@ -15,6 +18,7 @@
 #                               before they are forwarded to Foreman
 class foreman_proxy::plugin::openscap (
   $enabled                = $::foreman_proxy::plugin::openscap::params::enabled,
+  $version                = $::foreman_proxy::plugin::openscap::params::version,
   $listen_on              = $::foreman_proxy::plugin::openscap::params::listen_on,
   $openscap_send_log_file = $::foreman_proxy::plugin::openscap::params::openscap_send_log_file,
   $spooldir               = $::foreman_proxy::plugin::openscap::params::spooldir,
@@ -24,7 +28,9 @@ class foreman_proxy::plugin::openscap (
   validate_absolute_path($spooldir)
   validate_absolute_path($openscap_send_log_file)
 
-  foreman_proxy::plugin { 'openscap': } ->
+  foreman_proxy::plugin { 'openscap':
+    version => $version,
+  } ->
   foreman_proxy::settings_file { 'openscap':
     template_path => 'foreman_proxy/plugin/openscap.yml.erb',
     listen_on     => $listen_on,
