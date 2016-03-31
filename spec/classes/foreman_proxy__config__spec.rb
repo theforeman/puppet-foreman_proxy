71f060278571d1ef1fe81178ed99b9f97e898184
Michael Moll <kvedulv@kvedulv.de>
2016-03-31 16:46:51 +0200

    use helpers to restrict test OSes

--- spec/classes/foreman_proxy__config__spec.rb
+++ spec/classes/foreman_proxy__config__spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'foreman_proxy::config' do
   on_supported_os.each do |os, facts|
+    next if only_test_os() and not only_test_os.include?(os)
+    next if exclude_test_os() and exclude_test_os.include?(os)
     context "on #{os}" do
       let(:facts) { facts }
 
