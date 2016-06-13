1857a97ef34622a5c6070c84c1274d5fbe4e0fe6
Stephen Benjamin <stephen@redhat.com>
2016-06-13 15:39:50 -0400

    Support new architecture for REX proxy packages
    closes GH-258

--- manifests/plugin/dynflow/params.pp
+++ manifests/plugin/dynflow/params.pp
@@ -4,4 +4,6 @@ class foreman_proxy::plugin::dynflow::params {
   $listen_on         = 'https'
   $database_path     = '/var/lib/foreman-proxy/dynflow/dynflow.sqlite'
   $console_auth      = true
+  $core_listen       = '0.0.0.0'
+  $core_port         = 8008
 }
