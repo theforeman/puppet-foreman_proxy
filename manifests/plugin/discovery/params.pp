d5070dbcfeeaefbef20b987cc0535dd4d97d8a1e
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-07-19 11:10:47 +0100

    Remove dependencies on external modules

--- manifests/plugin/discovery/params.pp
+++ manifests/plugin/discovery/params.pp
@@ -1,9 +1,9 @@
 # Default parameters for foreman_proxy::plugin::discovery
 class foreman_proxy::plugin::discovery::params {
-  include ::tftp::params
+  include ::foreman_proxy::params
 
   $install_images = false
-  $tftp_root      = $::tftp::params::root
+  $tftp_root      = $::foreman_proxy::params::tftp_root
   $source_url     = 'http://downloads.theforeman.org/discovery/releases/latest/'
   $image_name     = 'fdi-image-latest.tar'
 }
