d5794d8d80b7a715be7ca0108f47ebaba7c4452b
Lukas Zapletal <lzap+git@redhat.com>
2016-09-01 11:27:57 +0200

    generate files for UEFI Grub/Grub2 support
    closes GH-276

--- manifests/tftp.pp
+++ manifests/tftp.pp
@@ -29,4 +29,92 @@ class foreman_proxy::tftp (
       root => $foreman_proxy::tftp_root,
     }
   }
+
+  case $::osfamily {
+    'RedHat': {
+      $osreleasemajor = regsubst($::operatingsystemrelease, '^(\d+)\..*$', '\1') # workaround for the possibly missing operatingsystemmajrelease
+      if versioncmp($osreleasemajor, '6') <= 0 {
+        $grub_type = 'redhat_old'
+      } else {
+        $grub_type = 'redhat'
+      }
+      # taken from http://pkgs.fedoraproject.org/cgit/rpms/grub2.git/tree/grub2.spec
+      $grub_modules = 'all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gfxmenu gfxterm gzio halt hfsplus iso9660 jpeg loadenv loopback lvm mdraid09 mdraid1x minicmd normal part_apple part_msdos part_gpt password_pbkdf2 png reboot search search_fs_uuid search_fs_file search_label serial sleep syslinuxcfg test tftp video xfs linux backtrace usb usbserial_common usbserial_pl2303 usbserial_ftdi usbserial_usbdebug linuxefi'
+    }
+    'Debian': {
+      $grub_type = 'debian'
+      # taken from https://anonscm.debian.org/cgit/pkg-grub/grub.git/tree/debian/build-efi-images
+      $grub_modules = 'all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio halt hfsplus iso9660 jpeg keystatus loadenv linux lsefi lsefimmap lsefisystab lssal memdisk minicmd normal part_apple part_msdos part_gpt password_pbkdf2 png reboot search search_fs_uuid search_fs_file search_label sleep test true video zfs zfscrypt zfsinfo linuxefi lvm mdraid09 mdraid1x raid5rec raid6rec tftp'
+    }
+    default: {
+      warning("Unable to detect EFI loader for OS family '${::osfamily}'")
+      $grub_type = 'unknown'
+    }
+  }
+
+  $efi_dir = '/usr/lib/grub/x86_64-efi'
+  case $::operatingsystem {
+    'Fedora': {
+      $grub_efi_path = 'fedora'
+    }
+    'CentOS': {
+      $grub_efi_path = 'centos'
+    }
+    /^(RedHat|Scientific|OracleLinux)$/: {
+      $grub_efi_path = 'redhat'
+    }
+    default: {
+      $grub_efi_path = 'unknown'
+    }
+  }
+
+  case $grub_type {
+    'redhat': {
+      ensure_packages(['grub2-efi','grub2-efi-modules','grub2-tools','shim'], { ensure => 'installed', })
+
+      exec {'build-grub2-efi-image':
+        command => "/usr/bin/grub2-mkimage -O x86_64-efi -d ${efi_dir} -o ${foreman_proxy::tftp_root}/grub2/grubx64.efi -p '' ${grub_modules}",
+        creates => "${foreman_proxy::tftp_root}/grub2/grubx64.efi",
+        require => [File[$foreman_proxy::tftp_dirs], Package['grub2-tools']],
+      }
+
+      foreman_proxy::tftp::copy_file{"/boot/efi/EFI/${grub_efi_path}/shim.efi":
+        target_path => "${foreman_proxy::tftp_root}/grub2",
+        require     => File[$foreman_proxy::tftp_dirs],
+      }
+    }
+    'redhat_old': {
+      ensure_packages(['grub'], { ensure => 'installed', })
+
+      file {"${foreman_proxy::tftp_root}/grub/grubx64.efi":
+        ensure  => file,
+        source  => "/boot/efi/EFI/${grub_efi_path}/grub.efi",
+        require => File[$foreman_proxy::tftp_dirs],
+      }
+
+      file {"${foreman_proxy::tftp_root}/grub/shim.efi":
+        ensure  => 'link',
+        target  => 'grubx64.efi',
+        require => File[$foreman_proxy::tftp_dirs],
+      }
+    }
+    'debian': {
+      ensure_packages(['grub-common','grub-efi-amd64-bin'], { ensure => 'installed', })
+
+      exec {'build-grub2-efi-image':
+        command => "/usr/bin/grub-mkimage -O x86_64-efi -d ${efi_dir} -o ${foreman_proxy::tftp_root}/grub2/grubx64.efi -p '' ${grub_modules}",
+        creates => "${foreman_proxy::tftp_root}/grub2/grubx64.efi",
+        require => [File[$foreman_proxy::tftp_dirs], Package['grub-common','grub-efi-amd64-bin']],
+      }
+
+      file {"${foreman_proxy::tftp_root}/grub2/shim.efi":
+        ensure  => 'link',
+        target  => 'grubx64.efi',
+        require => File[$foreman_proxy::tftp_dirs],
+      }
+    }
+    default: {
+      warning('UEFI PXE firmware is not being deployed')
+    }
+  }
 }
