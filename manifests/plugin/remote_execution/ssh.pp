d2a83c0d7d4446a5bd25dff2bf3dbba2b2a7e30d
Adam Ruzicka <aruzicka@redhat.com>
2017-05-31 14:35:18 +0200

    Fixes #11936 - Add support for kerberos auth

--- manifests/plugin/remote_execution/ssh.pp
+++ manifests/plugin/remote_execution/ssh.pp
@@ -15,6 +15,8 @@
 #
 # $ssh_keygen::         Location of the ssh-keygen binary
 #
+# $ssh_kerberos_auth::  Enable kerberos authentication for SSH
+#
 # $local_working_dir::  Local working directory on the smart proxy
 #
 # $remote_working_dir:: Remote working directory on clients
@@ -35,6 +37,7 @@ class foreman_proxy::plugin::remote_execution::ssh (
   String $ssh_keygen = $::foreman_proxy::plugin::remote_execution::ssh::params::ssh_keygen,
   Stdlib::Absolutepath $local_working_dir = $::foreman_proxy::plugin::remote_execution::ssh::params::local_working_dir,
   Stdlib::Absolutepath $remote_working_dir = $::foreman_proxy::plugin::remote_execution::ssh::params::remote_working_dir,
+  Boolean $ssh_kerberos_auth = $::foreman_proxy::plugin::remote_execution::ssh::params::ssh_kerberos_auth,
 ) inherits foreman_proxy::plugin::remote_execution::ssh::params {
 
   $ssh_identity_path = "${ssh_identity_dir}/${ssh_identity_file}"
@@ -47,6 +50,18 @@ class foreman_proxy::plugin::remote_execution::ssh (
     enabled       => $enabled,
     listen_on     => $listen_on,
     template_path => 'foreman_proxy/plugin/remote_execution_ssh.yml.erb',
+  }
+
+  if $ssh_kerberos_auth {
+    if $::osfamily == 'RedHat' and $::operatingsystem != 'Fedora' {
+      $scl_prefix = 'tfm-'
+    } else {
+      $scl_prefix = '' # lint:ignore:empty_string_assignment
+    }
+
+    foreman_proxy::plugin { 'net-ssh-krb':
+      package => "${scl_prefix}rubygem-net-ssh-krb"
+    }
   }
 
   if $generate_keys {
