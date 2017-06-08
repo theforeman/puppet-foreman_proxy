8e5d4c8f1dac7f4a6c12ee4a9943650aec51fbe0
Sean O'Keeffe <seanokeeffe797@gmail.com>
2017-06-08 21:22:15 +0100

    Fixes #19956 Add ssl_disabled_ciphers dynflow_core

--- spec/classes/foreman_proxy__plugin__dynflow_spec.rb
+++ spec/classes/foreman_proxy__plugin__dynflow_spec.rb
@@ -41,4 +41,40 @@ describe 'foreman_proxy::plugin::dynflow' do
       ])
     end
   end
+
+  describe 'with custom settings' do
+    let :facts do
+      on_supported_os['redhat-7-x86_64']
+    end
+
+    let :pre_condition do
+      "include foreman_proxy"
+    end
+    let :params do {
+      :ssl_disabled_ciphers => ['NULL-MD5', 'NULL-SHA'],
+    } end
+
+    it { should contain_foreman_proxy__plugin('dynflow') }
+
+    it 'should create settings.d symlink' do
+      should contain_file("/etc/smart_proxy_dynflow_core/settings.d").
+        with_ensure('link').with_target('/etc/foreman-proxy/settings.d')
+    end
+
+    it 'should generate correct dynflow core settings.yml' do
+      verify_exact_contents(catalogue, "/etc/smart_proxy_dynflow_core/settings.yml", [
+          "---",
+          ":database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite",
+          ":console_auth: true",
+          ":foreman_url: https://foo.example.com",
+          ":listen: 0.0.0.0",
+          ":port: 8008",
+          ":use_https: true",
+          ":ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem",
+          ":ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem",
+          ":ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem",
+          ':ssl_disabled_ciphers: ["NULL-MD5", "NULL-SHA"]'
+      ])
+    end
+  end
 end
