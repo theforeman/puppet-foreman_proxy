5b1e2e4bea5a09a32f0aa74f787b139616722441
Sean O'Keeffe <seanokeeffe797@gmail.com>
2017-06-08 21:22:15 +0100

    Fixes #19956 Add ssl_disabled_ciphers dynflow_core

--- manifests/plugin/dynflow/params.pp
+++ manifests/plugin/dynflow/params.pp
@@ -1,9 +1,10 @@
 # Default parameters for the Dynflow smart proxy plugin
 class foreman_proxy::plugin::dynflow::params {
-  $enabled           = true
-  $listen_on         = 'https'
-  $database_path     = '/var/lib/foreman-proxy/dynflow/dynflow.sqlite'
-  $console_auth      = true
-  $core_listen       = '0.0.0.0'
-  $core_port         = 8008
+  $enabled              = true
+  $listen_on            = 'https'
+  $database_path        = '/var/lib/foreman-proxy/dynflow/dynflow.sqlite'
+  $console_auth         = true
+  $core_listen          = '0.0.0.0'
+  $core_port            = 8008
+  $ssl_disabled_ciphers = undef
 }
