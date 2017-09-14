b4c47206d0e2c32179eb2e1b3d00933d97a634ad
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-09-14 12:18:13 +0200

    Fix TFTP grub packages for EL 7.4+

--- manifests/tftp.pp
+++ manifests/tftp.pp
@@ -70,7 +70,11 @@ class foreman_proxy::tftp {
 
   case $grub_type {
     'redhat': {
-      ensure_packages(['grub2-efi','grub2-efi-modules','grub2-tools','shim'], { ensure => 'installed', })
+      if versioncmp($::operatingsystemrelease, '7.4') >= 0 and $::operatingsystem != 'Fedora' {
+        ensure_packages(['grub2-efi-x64','grub2-efi-x64-modules','grub2-tools','shim-x64'], { ensure => 'installed', })
+      } else {
+        ensure_packages(['grub2-efi','grub2-efi-modules','grub2-tools','shim'], { ensure => 'installed', })
+      }
 
       foreman_proxy::tftp::copy_file{"/boot/efi/EFI/${grub_efi_path}/grubx64.efi":
         target_path => "${foreman_proxy::tftp_root}/grub2",
