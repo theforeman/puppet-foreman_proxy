765399bfa52bd203cee5df20fd7e51960f1a179c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-10-02 17:50:25 +0200

    Refs #21006 - Workaround broken grub2 on EL7.4

--- manifests/tftp.pp
+++ manifests/tftp.pp
@@ -28,6 +28,9 @@ class foreman_proxy::tftp {
     ensure_packages(['wget'], { ensure => $foreman_proxy::ensure_packages_version, })
   }
 
+  # taken from https://anonscm.debian.org/cgit/pkg-grub/grub.git/tree/debian/build-efi-images + regexp
+  $grub_modules = 'all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio halt hfsplus iso9660 jpeg keystatus loadenv linux lsefi lsefimmap lsefisystab lssal memdisk minicmd normal part_apple part_msdos part_gpt password_pbkdf2 png reboot search search_fs_uuid search_fs_file search_label sleep test true video zfs zfscrypt zfsinfo linuxefi lvm mdraid09 mdraid1x raid5rec raid6rec tftp regexp'
+
   case $::osfamily {
     'RedHat': {
       $osreleasemajor = regsubst($::operatingsystemrelease, '^(\d+)\..*$', '\1') # workaround for the possibly missing operatingsystemmajrelease
@@ -39,8 +42,6 @@ class foreman_proxy::tftp {
     }
     'Debian': {
       $grub_type = 'debian'
-      # taken from https://anonscm.debian.org/cgit/pkg-grub/grub.git/tree/debian/build-efi-images + regexp
-      $grub_modules = 'all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio halt hfsplus iso9660 jpeg keystatus loadenv linux lsefi lsefimmap lsefisystab lssal memdisk minicmd normal part_apple part_msdos part_gpt password_pbkdf2 png reboot search search_fs_uuid search_fs_file search_label sleep test true video zfs zfscrypt zfsinfo linuxefi lvm mdraid09 mdraid1x raid5rec raid6rec tftp regexp'
     }
     default: {
       warning("Unable to detect EFI loader for OS family '${::osfamily}'")
@@ -71,14 +72,28 @@ class foreman_proxy::tftp {
   case $grub_type {
     'redhat': {
       if versioncmp($::operatingsystemrelease, '7.4') >= 0 and $::operatingsystem != 'Fedora' {
-        ensure_packages(['grub2-efi-x64','grub2-efi-x64-modules','grub2-tools','shim-x64'], { ensure => 'installed', })
+        $packages = ['grub2-efi-x64','grub2-efi-x64-modules','grub2-tools','shim-x64']
+        $efi_lib_dir = '/usr/lib/grub/x86_64-efi'
+
+        ensure_packages($packages, { ensure => 'installed', })
+
+        # RHEL 7.4 renamed the packages and introduced a regression in the MAC loading of configs
+        exec { 'build-grub2-efi-image':
+          command => "/usr/bin/grub2-mkimage -O x86_64-efi -d ${efi_lib_dir} -o ${foreman_proxy::tftp_root}/grub2/grubx64.efi -p '' ${grub_modules}",
+          unless  => "/bin/grep -q regexp '${foreman_proxy::tftp_root}/grub2/grubx64.efi'",
+          require => Package[$packages],
+        }
+        -> file { "${foreman_proxy::tft_root}/grub2/grubx64.efi":
+          mode  => '0644',
+          owner => 'root',
+        }
       } else {
         ensure_packages(['grub2-efi','grub2-efi-modules','grub2-tools','shim'], { ensure => 'installed', })
-      }
 
-      foreman_proxy::tftp::copy_file{"/boot/efi/EFI/${grub_efi_path}/grubx64.efi":
-        target_path => "${foreman_proxy::tftp_root}/grub2",
-        require     => File[$foreman_proxy::tftp_dirs],
+        foreman_proxy::tftp::copy_file{"/boot/efi/EFI/${grub_efi_path}/grubx64.efi":
+          target_path => "${foreman_proxy::tftp_root}/grub2",
+          require     => File[$foreman_proxy::tftp_dirs],
+        }
       }
 
       foreman_proxy::tftp::copy_file{"/boot/efi/EFI/${grub_efi_path}/shim.efi":
