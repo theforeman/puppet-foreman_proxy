9963a1d3414861ea5a4fa5ace6d3a1ba5dcadea9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-01-11 15:49:56 +0100

    Drop compatibility for 1.15 and older
    We can assume new installations. Users of older versions should read the
    notes on compatibility.

--- manifests/config.pp
+++ manifests/config.pp
@@ -92,10 +92,8 @@ class foreman_proxy::config {
     feature   => 'Realm',
     listen_on => $::foreman_proxy::realm_listen_on,
   }
-  if $::foreman_proxy::realm_split_config_files {
-    foreman_proxy::settings_file { 'realm_freeipa':
-      module => false,
-    }
+  foreman_proxy::settings_file { 'realm_freeipa':
+    module => false,
   }
   foreman_proxy::settings_file { 'tftp':
     enabled   => $::foreman_proxy::tftp,
