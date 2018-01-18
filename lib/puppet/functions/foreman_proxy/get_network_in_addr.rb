b3b11429d1a2b6372f7eb2d1c2bc17dc80a5ef18
Julian Todt <34890848+juliantodt@users.noreply.github.com>
2018-01-18 17:21:19 +0100

    Use Puppet 4 functions API
    Also, use Puppet datatypes in get_network_in_addr

---
+++ lib/puppet/functions/foreman_proxy/get_network_in_addr.rb
@@ -0,0 +1,30 @@
+# Get the in-addr.arpa notation for the given IP address and a netmask
+
+require 'ipaddr'
+
+Puppet::Functions.create_function(:'foreman_proxy::get_network_in_addr') do
+  dispatch :get_network_in_addr do
+    required_param 'Stdlib::Compat::Ipv4', :address
+    required_param 'Stdlib::Compat::Ipv4', :netmask
+  end
+
+  def get_network_in_addr(address_string, netmask_string)
+    address = IPAddr.new(address_string)
+    netmask = IPAddr.new(netmask_string)
+
+    # The following gets the bits from the netmask, so it turns /255.255.255.0 into
+    # /24. We then get the number of times we need to split. Here we rely on
+    # truncation (25 / 8 = 3). We then split 5 - 3 times so we end up with
+    # '0.1.168.192.in-addr.arpa'.split(".", 2) = ['0', '1.168.192.in-addr.arpa']
+    # The last part is our result.
+    bits = netmask.to_i.to_s(2).count("1")
+
+    bits = 31 if bits == 32
+    if bits < 8
+      raise ArgumentError.new("get_network_in_addr(): subnets smaller than /8 are not supported")
+    end
+
+    parts = 5 - (bits / 8)
+    return address.reverse.to_s.split(".", parts).last
+  end
+end
--- spec/functions/get_network_in_addr_spec.rb
+++ spec/functions/get_network_in_addr_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-describe 'get_network_in_addr' do
+describe 'foreman_proxy::get_network_in_addr' do
   it 'should exist' do
-    expect(Puppet::Parser::Functions.function('get_network_in_addr')).to eq 'function_get_network_in_addr'
+    is_expected.not_to eq(nil)
   end
 
   it 'should throw an error with bad number of arguments' do
-    is_expected.to run.with_params().and_raise_error(Puppet::ParseError)
-    is_expected.to run.with_params('192.168.1.0').and_raise_error(Puppet::ParseError)
-    is_expected.to run.with_params('192.168.1.0', '255.255.255.0', 'additional').and_raise_error(Puppet::ParseError)
+    is_expected.to run.with_params().and_raise_error(ArgumentError)
+    is_expected.to run.with_params('192.168.1.0').and_raise_error(ArgumentError)
+    is_expected.to run.with_params('192.168.1.0', '255.255.255.0', 'additional').and_raise_error(ArgumentError)
   end
 
   it 'should throw an error on invalid parameters' do
-    is_expected.to run.with_params('192.168.1', '255.255.255.0').and_raise_error(Puppet::ParseError)
-    is_expected.to run.with_params('2001:db8::1', '255.255.255.0').and_raise_error(Puppet::ParseError)
-    is_expected.to run.with_params('192.168.1.0', '32').and_raise_error(Puppet::ParseError)
-    is_expected.to run.with_params('192.168.1.0', '2001:db8::1').and_raise_error(Puppet::ParseError)
+    is_expected.to run.with_params('192.168.1', '255.255.255.0').and_raise_error(ArgumentError)
+    is_expected.to run.with_params('2001:db8::1', '255.255.255.0').and_raise_error(ArgumentError)
+    is_expected.to run.with_params('192.168.1.0', '32').and_raise_error(ArgumentError)
+    is_expected.to run.with_params('192.168.1.0', '2001:db8::1').and_raise_error(ArgumentError)
   end
 
   it 'should work on IPv4 /8 to /32' do
@@ -33,7 +33,7 @@ describe 'get_network_in_addr' do
 
   it 'should fail on IPv4 /0 to /7' do
     ['0.0.0.0.0', '128.0.0.0', '192.0.0.0', '224.0.0.0', '240.0.0.0', '248.0.0.0', '252.0.0.0', '254.0.0.0'].each do |netmask|
-      is_expected.to run.with_params('192.168.1.0', netmask).and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params('192.168.1.0', netmask).and_raise_error(ArgumentError)
     end
   end
 end
