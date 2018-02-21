7e3ed8b0467fa1709ecb624a1de1a277650bb1a0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-21 20:17:03 +0100

    Run dynflow tests on all operating systems

--- spec/classes/foreman_proxy__plugin__dynflow_spec.rb
+++ spec/classes/foreman_proxy__plugin__dynflow_spec.rb
@@ -1,83 +1,110 @@
 require 'spec_helper'
 
 describe 'foreman_proxy::plugin::dynflow' do
-  describe 'with default settings' do
-    let :facts do
-      on_supported_os['redhat-7-x86_64']
-    end
+  on_os_under_test.each do |os, facts|
+    context "on #{os}" do
+      let :facts do
+        facts
+      end
 
-    let :pre_condition do
-      "include foreman_proxy"
-    end
+      let :pre_condition do
+        "include foreman_proxy"
+      end
 
-    it { should contain_foreman_proxy__plugin('dynflow') }
+      let :etc_dir do
+        case facts[:osfamily]
+        when 'FreeBSD', 'DragonFly'
+          '/usr/local/etc'
+        else
+          '/etc'
+        end
+      end
 
-    it 'should generate correct dynflow.yml' do
-      verify_exact_contents(catalogue, "/etc/foreman-proxy/settings.d/dynflow.yml", [
-          '---',
-          ':enabled: https',
-          ':database: ',
-          ':core_url: https://foo.example.com:8008',
-      ])
-    end
+      has_core = facts[:osfamily] == 'RedHat' && facts[:operatingsystem] != 'Fedora'
 
-    it 'should create settings.d symlink' do
-      should contain_file("/etc/smart_proxy_dynflow_core/settings.d").
-        with_ensure('link').with_target('/etc/foreman-proxy/settings.d')
-    end
+      describe 'with default settings' do
+        it { should compile.with_all_deps }
+        it { should contain_foreman_proxy__plugin('dynflow') }
 
-    it 'should generate correct dynflow core settings.yml' do
-      verify_exact_contents(catalogue, "/etc/smart_proxy_dynflow_core/settings.yml", [
-          "---",
-          ":database: ",
-          ":console_auth: true",
-          ":foreman_url: https://foo.example.com",
-          ":listen: 0.0.0.0",
-          ":port: 8008",
-          ":use_https: true",
-          ":ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem",
-          ":ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem",
-          ":ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem"
-      ])
-    end
-  end
+        it 'should generate correct dynflow.yml' do
+          verify_exact_contents(catalogue, "#{etc_dir}/foreman-proxy/settings.d/dynflow.yml", [
+            '---',
+            ':enabled: https',
+            ':database: ',
+            ':core_url: https://foo.example.com:8008',
+          ])
+        end
 
-  describe 'with custom settings' do
-    let :facts do
-      on_supported_os['redhat-7-x86_64']
-    end
+        if has_core
+          it { should contain_foreman_proxy__plugin('dynflow_core') }
+          it { should contain_service('smart_proxy_dynflow_core') }
 
-    let :pre_condition do
-      "include foreman_proxy"
-    end
-    let :params do {
-      :database_path         => '/var/lib/foreman-proxy/dynflow/dynflow.sqlite',
-      :ssl_disabled_ciphers  => ['NULL-MD5', 'NULL-SHA'],
-      :tls_disabled_versions => ['1.1']
-    } end
+          it 'should create settings.d symlink' do
+            should contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d").
+              with_ensure('link').with_target("#{etc_dir}/foreman-proxy/settings.d")
+          end
 
-    it { should contain_foreman_proxy__plugin('dynflow') }
+          it 'should generate correct dynflow core settings.yml' do
+            verify_exact_contents(catalogue, "#{etc_dir}/smart_proxy_dynflow_core/settings.yml", [
+              "---",
+              ":database: ",
+              ":console_auth: true",
+              ":foreman_url: https://foo.example.com",
+              ":listen: 0.0.0.0",
+              ":port: 8008",
+              ":use_https: true",
+              ":ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem",
+              ":ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem",
+              ":ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem"
+            ])
+          end
+        else
+          it { should_not contain_foreman_proxy__plugin('dynflow_core') }
+          it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d") }
+          it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.yml") }
+          it { should_not contain_service('smart_proxy_dynflow_core') }
+        end
+      end
 
-    it 'should create settings.d symlink' do
-      should contain_file("/etc/smart_proxy_dynflow_core/settings.d").
-        with_ensure('link').with_target('/etc/foreman-proxy/settings.d')
-    end
+      describe 'with custom settings' do
+        let :params do {
+          :database_path         => '/var/lib/foreman-proxy/dynflow/dynflow.sqlite',
+          :ssl_disabled_ciphers  => ['NULL-MD5', 'NULL-SHA'],
+          :tls_disabled_versions => ['1.1']
+        } end
 
-    it 'should generate correct dynflow core settings.yml' do
-      verify_exact_contents(catalogue, "/etc/smart_proxy_dynflow_core/settings.yml", [
-        '---',
-        ':database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite',
-        ':console_auth: true',
-        ':foreman_url: https://foo.example.com',
-        ':listen: 0.0.0.0',
-        ':port: 8008',
-        ':use_https: true',
-        ':ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem',
-        ':ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem',
-        ':ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem',
-        ':ssl_disabled_ciphers: ["NULL-MD5", "NULL-SHA"]',
-        ':tls_disabled_versions: ["1.1"]',
-      ])
+        it { should compile.with_all_deps }
+        it { should contain_foreman_proxy__plugin('dynflow') }
+
+        if has_core
+          it 'should create settings.d symlink' do
+            should contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d").
+              with_ensure('link').with_target("#{etc_dir}/foreman-proxy/settings.d")
+          end
+
+          it 'should generate correct dynflow core settings.yml' do
+            verify_exact_contents(catalogue, "#{etc_dir}/smart_proxy_dynflow_core/settings.yml", [
+              '---',
+              ':database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite',
+              ':console_auth: true',
+              ':foreman_url: https://foo.example.com',
+              ':listen: 0.0.0.0',
+              ':port: 8008',
+              ':use_https: true',
+              ':ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem',
+              ':ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem',
+              ':ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem',
+              ':ssl_disabled_ciphers: ["NULL-MD5", "NULL-SHA"]',
+              ':tls_disabled_versions: ["1.1"]',
+            ])
+          end
+        else
+          it { should_not contain_foreman_proxy__plugin('dynflow_core') }
+          it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d") }
+          it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.yml") }
+          it { should_not contain_service('smart_proxy_dynflow_core') }
+        end
+      end
     end
   end
 end
