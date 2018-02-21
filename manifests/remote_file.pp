a24f8d8482ba76381f5c3f655bb87b147057600a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-21 18:29:18 +0100

    Fixes #22479 - Handle remote directory with undefined parent
    This can happen when tftp is set to false or unmanaged.
    
    We move the remote_file define to this module since puppet-foreman no
    longer uses it itself. That will allow us to remove it there in a next
    major release.

---
+++ manifests/remote_file.pp
@@ -0,0 +1,17 @@
+# Downloads a file from a URL to a local file given by the title
+define foreman_proxy::remote_file(
+  $remote_location,
+  $mode='0644',
+) {
+  $parent = dirname($title)
+  File <| title == $parent |>
+  -> exec { "mkdir -p ${parent}":
+    path    => ['/bin', '/usr/bin'],
+    creates => $parent,
+  }
+  -> file { $title:
+    source  => $remote_location,
+    mode    => $mode,
+    replace => false,
+  }
+}
--- spec/classes/foreman_proxy__plugin__discovery_spec.rb
+++ spec/classes/foreman_proxy__plugin__discovery_spec.rb
@@ -18,11 +18,12 @@ describe 'foreman_proxy::plugin::discovery' do
           tftproot = '/var/lib/tftpboot'
       end
 
-      it { should contain_foreman_proxy__plugin('discovery') }
-      it { should contain_foreman_proxy__feature('Discovery') }
-
       describe 'without paramaters' do
-        it { should_not contain_foreman__remote_file("#{tftproot}/boot/fdi-image-latest.tar") }
+        it { should compile.with_all_deps }
+        it { should contain_foreman_proxy__plugin('discovery') }
+        it { should contain_foreman_proxy__feature('Discovery') }
+        it { should_not contain_foreman_proxy__remote_file("#{tftproot}/boot/fdi-image-latest.tar") }
+        it { should_not contain_exec('untar fdi-image-latest.tar') }
       end
 
       describe 'with install_images => true' do
@@ -32,8 +33,12 @@ describe 'foreman_proxy::plugin::discovery' do
           }
         end
 
+        it { should compile.with_all_deps }
+        it { should contain_foreman_proxy__plugin('discovery') }
+        it { should contain_foreman_proxy__feature('Discovery') }
+
         it 'should download and install tarball' do
-          should contain_foreman__remote_file("#{tftproot}/boot/fdi-image-latest.tar").
+          should contain_foreman_proxy__remote_file("#{tftproot}/boot/fdi-image-latest.tar").
             with_remote_location('http://downloads.theforeman.org/discovery/releases/latest/fdi-image-latest.tar')
         end
 
