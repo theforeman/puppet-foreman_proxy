83b867221c003568a0360b58c9f45887a42ff757
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-21 20:16:29 +0100

    Clean up pulp plugin spec
    This removes checks for Puppet 3 which we've dropped and simplifies the
    actual testing.

--- spec/classes/foreman_proxy__plugin__pulp_spec.rb
+++ spec/classes/foreman_proxy__plugin__pulp_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe 'foreman_proxy::plugin::pulp' do
-
-  let :puppet_environmentpath do
-    # In Puppet 3, Puppet[:environmentpath] default is ""
-    Gem::Version.new(Puppet.version) >= Gem::Version.new('4.0') ? '/etc/puppetlabs/code/environments' : ""
+  let :facts do
+    on_supported_os['redhat-7-x86_64']
   end
 
-  let :facts do
-    on_supported_os['redhat-7-x86_64'].merge(:puppet_environmentpath => puppet_environmentpath)
+  let :pre_condition do
+    "include foreman_proxy"
   end
 
   let :etc_dir do
@@ -16,45 +14,42 @@ describe 'foreman_proxy::plugin::pulp' do
   end
 
   describe 'with default settings' do
-    let :pre_condition do
-      "include foreman_proxy"
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_foreman_proxy__plugin('pulp') }
+
+    it 'should configure pulp.yml' do
+      is_expected.to contain_file("#{etc_dir}/foreman-proxy/settings.d/pulp.yml")
+        .with_ensure('file')
+        .with_owner('root')
+        .with_group('foreman-proxy')
     end
 
-    it { should contain_foreman_proxy__plugin('pulp') }
-    it 'should configure pulp.yml' do
-      should contain_file('/etc/foreman-proxy/settings.d/pulp.yml').
-        with({
-          :ensure  => 'file',
-          :owner   => 'root',
-          :group   => 'foreman-proxy'
-        })
-        verify_exact_contents(catalogue, "#{etc_dir}/foreman-proxy/settings.d/pulp.yml", [
-          '---',
-          ':enabled: https',
-          ":pulp_url: https://#{facts[:fqdn]}/pulp",
-          ':pulp_dir: /var/lib/pulp',
-          ':pulp_content_dir: /var/lib/pulp/content',
-          ":puppet_content_dir: #{puppet_environmentpath.empty? ? '/etc/puppet/environments' : puppet_environmentpath}",
-          ':mongodb_dir: /var/lib/mongodb',
-        ])
+    it 'should generate correct pulp.yml' do
+      verify_exact_contents(catalogue, "#{etc_dir}/foreman-proxy/settings.d/pulp.yml", [
+        '---',
+        ':enabled: https',
+        ":pulp_url: https://#{facts[:fqdn]}/pulp",
+        ':pulp_dir: /var/lib/pulp',
+        ':pulp_content_dir: /var/lib/pulp/content',
+        ':puppet_content_dir: /etc/puppetlabs/code/environments',
+        ':mongodb_dir: /var/lib/mongodb',
+      ])
     end
   end
 
   describe 'with overrides' do
-    let :pre_condition do
-      "include foreman_proxy"
-    end
     let :params do {
       :group => 'example',
       :puppet_content_dir => '/tmp/foo',
     } end
 
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_foreman_proxy__plugin('pulp') }
+
     it 'should change pulp.yml group' do
-      should contain_file('/etc/foreman-proxy/settings.d/pulp.yml').
-        with({
-          :owner   => 'root',
-          :group   => 'example'
-        })
+      is_expected.to contain_file("#{etc_dir}/foreman-proxy/settings.d/pulp.yml")
+        .with_owner('root')
+        .with_group('example')
     end
 
     it 'should generate correct pulp.yml' do
