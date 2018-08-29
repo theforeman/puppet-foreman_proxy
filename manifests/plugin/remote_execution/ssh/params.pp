a3cb2c17cc4b910018add2d5100e789c7cc93f3c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-08-29 11:18:40 +0200

    Move the REX SSH directory to /var/lib/foreman-proxy
    Starting 0.2.0-2 for Debs and 1.5.2 for RPMs we've moved the .ssh
    directory to /var/lib/foreman-proxy/ssh with a symlink in
    /usr/share/foreman-proxy/.ssh.

--- manifests/plugin/remote_execution/ssh/params.pp
+++ manifests/plugin/remote_execution/ssh/params.pp
@@ -1,14 +1,12 @@
 # Remote Execution SSH default parameters
 class foreman_proxy::plugin::remote_execution::ssh::params {
-  include ::foreman_proxy::params
-
   $enabled            = true
   $listen_on          = 'https'
   $local_working_dir  = '/var/tmp'
   $remote_working_dir = '/var/tmp'
   $generate_keys      = true
   $install_key        = false
-  $ssh_identity_dir   = "${::foreman_proxy::params::dir}/.ssh"
+  $ssh_identity_dir   = '/var/lib/foreman-proxy/ssh'
   $ssh_identity_file  = 'id_rsa_foreman_proxy'
   $ssh_keygen         = '/usr/bin/ssh-keygen'
   $ssh_kerberos_auth  = false
