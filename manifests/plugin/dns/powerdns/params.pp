015b199e3d8618d7b2b1798fe6bcac158784d0ac
Michael Moll <kvedulv@kvedulv.de>
2018-10-11 21:30:55 +0200

    namespace extlib functions

--- manifests/plugin/dns/powerdns/params.pp
+++ manifests/plugin/dns/powerdns/params.pp
@@ -2,7 +2,7 @@ class foreman_proxy::plugin::dns::powerdns::params {
   $backend               = 'mysql'
   $mysql_hostname        = 'localhost'
   $mysql_username        = 'pdns'
-  $mysql_password        = cache_data('foreman_cache_data', 'smart_proxy_dns_powerdns_password', random_password(16))
+  $mysql_password        = extlib::cache_data('foreman_cache_data', 'smart_proxy_dns_powerdns_password', extlib::random_password(16))
   $mysql_database        = 'pdns'
   $postgresql_connection = '' # lint:ignore:empty_string_assignment
   $rest_url              = 'http://localhost:8081/api/v1/servers/localhost'
