6a315685f5fae0dc9e7c0f052ca2f3c9c5a071e6
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-04-17 15:36:27 +0200

    Add pulp3 options to the pulp plugin
    Since smart_proxy_pulp 1.4.0 pulp3.yml is created.

--- manifests/plugin/pulp.pp
+++ manifests/plugin/pulp.pp
@@ -21,6 +21,10 @@
 #
 # $pulpnode_enabled::   enables/disables the pulpnode plugin
 #
+# $pulp3_enabled::      enables/disables the pulp3 plugin
+#
+# $pulp3_mirror::       Whether this pulp3 plugin acts as a mirror or another pulp node. A pulp3 mirror is the pulp3 equivalent of a pulpnode.
+#
 # $puppet_content_dir:: directory for puppet content
 #
 # $mongodb_dir::        directory for Mongo DB
@@ -29,6 +33,8 @@ class foreman_proxy::plugin::pulp (
   Boolean $enabled = $::foreman_proxy::plugin::pulp::params::enabled,
   Foreman_proxy::ListenOn $listen_on = $::foreman_proxy::plugin::pulp::params::listen_on,
   Boolean $pulpnode_enabled = $::foreman_proxy::plugin::pulp::params::pulpnode_enabled,
+  Boolean $pulp3_enabled = $::foreman_proxy::plugin::pulp::params::pulp3_enabled,
+  Boolean $pulp3_mirror = $::foreman_proxy::plugin::pulp::params::pulp3_mirror,
   Optional[String] $version = $::foreman_proxy::plugin::pulp::params::version,
   Optional[String] $group = $::foreman_proxy::plugin::pulp::params::group,
   Stdlib::HTTPUrl $pulp_url = $::foreman_proxy::plugin::pulp::params::pulp_url,
@@ -50,6 +56,12 @@ class foreman_proxy::plugin::pulp (
     template_path => 'foreman_proxy/plugin/pulpnode.yml.erb',
     group         => $group,
     enabled       => $pulpnode_enabled,
+    listen_on     => $listen_on,
+  }
+  -> foreman_proxy::settings_file { 'pulp3':
+    template_path => 'foreman_proxy/plugin/pulp3.yml.erb',
+    group         => $group,
+    enabled       => $pulp3_enabled,
     listen_on     => $listen_on,
   }
 }
