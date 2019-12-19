74c9ab951c9134509502229112b112b76154caa1
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-19 17:07:43 +0100

    Restructure spec tests to inherit more context
    It also changes the structure of the proxydns facts to inherit more and
    be more explicit.

--- spec/classes/foreman_proxy__proxydns__spec.rb
+++ spec/classes/foreman_proxy__proxydns__spec.rb
@@ -3,24 +3,22 @@ require 'spec_helper'
 describe 'foreman_proxy::proxydns' do
   on_os_under_test.each do |os, facts|
     context "on #{os}" do
-      context 'with inherited parameters' do
-        let :facts do
-          facts.merge(
-            :ipaddress_eth0 => '192.168.100.1',
-            :netmask_eth0   => '255.255.255.0',
-          )
-        end
+      let :facts do
+        facts.merge(
+          ipaddress_eth0: '192.168.100.1',
+          netmask_eth0: '255.255.255.0',
+        )
+      end
 
-        let :pre_condition do
-          'include ::foreman_proxy'
-        end
+      context 'with inherited parameters' do
+        let(:pre_condition) { 'include foreman_proxy' }
 
         it { should compile.with_all_deps }
 
         it 'should inherit the correct parameters' do
           should contain_class('foreman_proxy::proxydns')
             .with_forwarders([])
-            .with_interface('eth0')
+            .with_interface(facts[:networking]['primary'] || 'eth0')
             .with_forward_zone('example.com')
             .with_reverse_zone(nil)
             .with_soa('foo.example.com')
@@ -28,7 +26,7 @@ describe 'foreman_proxy::proxydns' do
       end
 
       context 'with explicit parameters' do
-        let :base_params do
+        let :params do
           {
             forwarders: [],
             interface: 'eth0',
@@ -37,18 +35,7 @@ describe 'foreman_proxy::proxydns' do
           }
         end
 
-        context 'with base parameters' do
-          let :facts do
-            facts.merge(
-              :ipaddress_eth0 => '192.168.100.1',
-              :netmask_eth0   => '255.255.255.0',
-            )
-          end
-
-          let :params do
-            base_params
-          end
-
+        context 'with eth0' do
           it { should compile.with_all_deps }
 
           it 'should include the dns class' do
@@ -67,39 +54,60 @@ describe 'foreman_proxy::proxydns' do
               .with_soa('foo.example.com')
               .with_reverse(true)
           end
-        end
 
-        context 'with dns_zone overridden' do
-          let :facts do
-            facts.merge(
-              :ipaddress_eth0 => '192.168.100.1',
-              :netmask_eth0   => '255.255.255.0',
-            )
+          context 'with dns_zone overridden' do
+            let(:params) { super().merge(forward_zone: 'something.example.com') }
+
+            it 'should include the forward zone' do
+              should contain_dns__zone('something.example.com')
+                .with_soa('foo.example.com')
+                .with_reverse(false)
+                .with_soaip('192.168.100.1')
+            end
           end
 
-          let :params do
-            base_params.merge(:forward_zone => 'something.example.com')
+          context "with dns_reverse value" do
+            let(:params) { super().merge(reverse_zone: ['168.192.in-addr.arpa']) }
+
+            it 'should include the reverse zone 168.192.in-addr.arpa' do
+              should contain_dns__zone('168.192.in-addr.arpa')
+                .with_soa('foo.example.com')
+                .with_reverse(true)
+            end
           end
 
-          it 'should include the forward zone' do
-            should contain_dns__zone('something.example.com')
-              .with_soa('foo.example.com')
-              .with_reverse(false)
-              .with_soaip('192.168.100.1')
+          context "with dns_reverse array" do
+            let(:params) { super().merge(reverse_zone: ['0.168.192.in-addr.arpa', '1.168.192.in-addr.arpa']) }
+
+            it 'should include the reverse zone 0.168.192.in-addr.arpa' do
+              should contain_dns__zone('0.168.192.in-addr.arpa')
+                .with_soa('foo.example.com')
+                .with_reverse(true)
+            end
+
+            it 'should include the reverse zone 1.168.192.in-addr.arpa' do
+              should contain_dns__zone('1.168.192.in-addr.arpa')
+                .with_soa('foo.example.com')
+                .with_reverse(true)
+            end
+          end
+
+          context 'invalid netmask fact' do
+            let(:facts) { super().merge(netmask_eth0: '0.0.0.0') }
+
+            it { should compile.and_raise_error(%r{subnets smaller than /8 are not supported}) }
           end
         end
 
         context "with vlan interface" do
           let :facts do
             facts.merge(
-              :ipaddress_eth0_0 => '192.168.100.1',
-              :netmask_eth0_0   => '255.255.255.0',
+              ipaddress_eth0_0: '192.168.100.1',
+              netmask_eth0_0: '255.255.255.0',
             )
           end
 
-          let :params do
-            base_params.merge(:interface => 'eth0:0')
-          end
+          let(:params) { super().merge(interface: 'eth0:0') }
 
           it 'should include the forward zone' do
             should contain_dns__zone('example.com')
@@ -118,14 +126,12 @@ describe 'foreman_proxy::proxydns' do
         context "with alias interface" do
           let :facts do
             facts.merge(
-              :ipaddress_eth0_0 => '192.168.100.1',
-              :netmask_eth0_0   => '255.255.255.0',
+              ipaddress_eth0_0: '192.168.100.1',
+              netmask_eth0_0: '255.255.255.0',
             )
           end
 
-          let :params do
-            base_params.merge(:interface => 'eth0:0')
-          end
+          let(:params) { super().merge(interface: 'eth0:0') }
 
           it 'should include the forward zone' do
             should contain_dns__zone('example.com')
@@ -142,87 +148,14 @@ describe 'foreman_proxy::proxydns' do
         end
 
         context 'with invalid interface' do
-          let :facts do
-            facts
-          end
+          let(:params) { super().merge(interface: 'invalid') }
 
-          let :params do
-            base_params.merge(:interface => 'invalid')
-          end
+          it { should compile.and_raise_error(/Could not get a valid IP address from fact ipaddress_invalid: '' \(Undef\)/) }
 
-          it { should raise_error(Puppet::Error, /Could not get a valid IP address from fact ipaddress_invalid: '' \(Undef\)/) }
-        end
-
-        context "with dns_reverse value" do
-          let :facts do
-            facts.merge(
-              :ipaddress_eth0 => '192.168.100.1',
-              :netmask_eth0   => '255.255.255.0',
-            )
-          end
-
-          let :params do
-            base_params.merge(:reverse_zone => ['168.192.in-addr.arpa'])
-          end
-
-          it 'should include the reverse zone 168.192.in-addr.arpa' do
-            should contain_dns__zone('168.192.in-addr.arpa')
-              .with_soa('foo.example.com')
-              .with_reverse(true)
-          end
-        end
-
-        context "with dns_reverse array" do
-          let :facts do
-            facts.merge(
-              :ipaddress_eth0 => '192.168.100.1',
-              :netmask_eth0   => '255.255.255.0',
-            )
-          end
-
-          let :params do
-            base_params.merge(:reverse_zone => ['0.168.192.in-addr.arpa', '1.168.192.in-addr.arpa'])
-          end
-
-          it 'should include the reverse zone 0.168.192.in-addr.arpa' do
-            should contain_dns__zone('0.168.192.in-addr.arpa')
-              .with_soa('foo.example.com')
-              .with_reverse(true)
-          end
-
-          it 'should include the reverse zone 1.168.192.in-addr.arpa' do
-            should contain_dns__zone('1.168.192.in-addr.arpa')
-              .with_soa('foo.example.com')
-              .with_reverse(true)
-          end
-        end
-
-        context 'with an invalid reverse' do
           context 'missing netmask fact' do
-            let :facts do
-              facts.merge(ipaddress_invalid: '192.0.2.1')
-            end
+            let(:facts) { facts.merge(ipaddress_invalid: '192.0.2.1') }
 
-            let :params do
-              base_params.merge(:interface => 'invalid')
-            end
-
-            it { should raise_error(Puppet::Error, /Could not get a valid netmask from fact netmask_invalid: '' \(Undef\)/) }
-          end
-
-          context 'invalid netmask fact' do
-            let :facts do
-              facts.merge(
-                :ipaddress_eth0 => '192.168.100.1',
-                :netmask_eth0   => '0.0.0.0',
-              )
-            end
-
-            let :params do
-              base_params.merge(:interface => 'eth0')
-            end
-
-            it { should raise_error(Puppet::Error) }
+            it { should compile.and_raise_error(/Could not get a valid netmask from fact netmask_invalid: '' \(Undef\)/) }
           end
         end
       end
