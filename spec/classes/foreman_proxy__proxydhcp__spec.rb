a7623d5dfb8bb1125de98a06ebc63a69b88a20d6
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-20 13:39:43 +0100

    Rewrite proxydhcp tests
    Uses inheritance of parameters and TEST-NET-[1-3] IPs. Also disables
    some other modules to get faster compiles.

--- spec/classes/foreman_proxy__proxydhcp__spec.rb
+++ spec/classes/foreman_proxy__proxydhcp__spec.rb
@@ -1,365 +1,226 @@
 require 'spec_helper'
 
-describe 'foreman_proxy::proxydhcp' do
+describe 'foreman_proxy' do
   on_os_under_test.each do |os, facts|
     context "on #{os}" do
 
+      let(:params) do
+        {
+          dhcp: true,
+          dhcp_managed: true,
+          dhcp_provider: 'isc',
+          dhcp_gateway: '192.0.2.1',
+          # Disable some integration for faster compiles
+          dns: false,
+          tftp: false,
+          puppet: false,
+          puppetca: false,
+        }
+      end
+
       context "on physical interface" do
         let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
+          facts.merge(
+            ipaddress: '192.0.2.10',
+            ipaddress_eth0: '192.0.2.10',
+            netmask_eth0: '255.255.255.0',
+            network_eth0: '192.0.2.0',
+          )
         end
 
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway => '127.0.0.254',
-          }"
+        let(:params) { super().merge(dhcp_interface: 'eth0') }
+
+        it do
+          should contain_class('dhcp')
+            .with_dnsdomain(['example.com'])
+            .with_nameservers(['192.0.2.10'])
+            .with_interfaces(['eth0'])
+            .with_pxeserver('192.0.2.10')
+            .with_pxefilename('pxelinux.0')
         end
 
-        it do should contain_class('dhcp').with(
-          'dnsdomain'   => ['example.com'],
-          'nameservers' => ['127.0.1.1'],
-          'interfaces'  => ['eth0'],
-          'pxeserver'   => '127.0.1.1',
-          'pxefilename' => 'pxelinux.0'
-        ) end
-
-        it do should contain_dhcp__pool('example.com').with(
-          'network'  => '127.0.0.0',
-          'mask'     => '255.0.0.0',
-          'range'    => nil,
-          'gateway'  => '127.0.0.254',
-          'failover' => nil
-        ) end
+        it do
+          should contain_dhcp__pool('example.com')
+            .with_network('192.0.2.0')
+            .with_mask('255.255.255.0')
+            .with_range(nil)
+            .with_gateway('192.0.2.1')
+            .with_failover(nil)
+        end
 
         it { should_not contain_class('dhcp::failover') }
+
+        context "as manager of ACLs for dhcp", unless: ['FreeBSD', 'DragonFly'].include?(facts[:osfamily]) do
+          let(:params) { super().merge(dhcp_manage_acls: true) }
+
+          it do should contain_exec('Allow foreman-proxy to read /etc/dhcp').
+            with_command("setfacl -R -m u:foreman-proxy:rx /etc/dhcp")
+          end
+
+          it do should contain_exec('Allow foreman-proxy to read /var/lib/dhcpd').
+            with_command("setfacl -R -m u:foreman-proxy:rx /var/lib/dhcpd")
+          end
+        end
+
+        context "as manager of ACLs for dhcp for RedHat only by default" do
+          case facts[:osfamily]
+          when 'RedHat'
+            it do should contain_exec('Allow foreman-proxy to read /etc/dhcp').
+              with_command('setfacl -R -m u:foreman-proxy:rx /etc/dhcp').
+              with_unless('getfacl -p /etc/dhcp | grep user:foreman-proxy:r-x')
+            end
+          else
+            it { should_not contain_exec('Allow foreman-proxy to read /etc/dhcp') }
+          end
+
+          case facts[:osfamily]
+          when 'RedHat'
+            it do should contain_exec('Allow foreman-proxy to read /var/lib/dhcpd').
+              with_command("setfacl -R -m u:foreman-proxy:rx /var/lib/dhcpd").
+              with_unless('getfacl -p /var/lib/dhcpd | grep user:foreman-proxy:r-x')
+            end
+          else
+            it { should_not contain_exec('Allow foreman-proxy to read /var/lib/dhcpd') }
+          end
+        end
+
+        context "with additional dhcp listen interfaces" do
+          let(:params) { super().merge(dhcp_additional_interfaces: [ 'vlan8', 'vlan9', 'vlan120' ]) }
+
+          it { should contain_class('dhcp').with_interfaces(['eth0', 'vlan8', 'vlan9', 'vlan120' ]) }
+        end
+
+        context "with one additional dhcp listen interface" do
+          let(:params) { super().merge(dhcp_additional_interfaces: ['vlan83']) }
+
+          it { should contain_class('dhcp').with_interfaces(['eth0', 'vlan83']) }
+        end
+
+        context "with additional dhcp listen interfaces wrongly specified as String data type" do
+          let(:params) { super().merge(dhcp_additional_interfaces: 'vlan55') }
+
+          it { should compile.and_raise_error(/expects an Array value, got String/) }
+        end
+
+        context "with additional dhcp listen interfaces wrongly specified as Hash data type" do
+          let(:params) { super().merge(dhcp_additional_interfaces: { name: 'vlan55' }) }
+
+          it { should compile.and_raise_error(/parameter 'dhcp_additional_interfaces' expects an Array value, got Struct/) }
+        end
+
+        context "with dhcp_search_domains" do
+          let(:params) { super().merge(dhcp_search_domains: ['example.com', 'example.org']) }
+
+          it { should contain_dhcp__pool('example.com').with_search_domains(['example.com','example.org']) }
+        end
+
+        context "with dhcp_pxeserver" do
+          let(:params) { super().merge(dhcp_pxeserver: '203.0.113.10') }
+
+          it { should contain_class('dhcp').with_pxeserver('203.0.113.10') }
+        end
+
+        context "as primary dhcp server" do
+          let(:params) do
+            super().merge(
+              dhcp_node_type: 'primary',
+              dhcp_peer_address: '203.0.113.40',
+            )
+          end
+
+          it do
+            should contain_class('dhcp::failover')
+              .with_role('primary')
+              .with_peer_address('203.0.113.40')
+              .with_address('192.0.2.10')
+          end
+        end
+
+        context "as secondary dhcp server" do
+          let(:params) do
+            super().merge(
+              dhcp_node_type: 'secondary',
+              dhcp_peer_address: '203.0.113.50',
+            )
+          end
+
+          it do
+            should contain_class('dhcp::failover')
+              .with_role('secondary')
+              .with_peer_address('203.0.113.50')
+              .with_address('192.0.2.10')
+          end
+        end
       end
 
       context "on vlan interface" do
         let :facts do
-          facts.merge({:ipaddress_eth0_0 => '127.0.1.1',
-                       :netmask_eth0_0   => '255.0.0.0',
-                       :network_eth0_0   => '127.0.0.0'})
+          facts.merge(
+            ipaddress_eth0_0: '203.0.113.10',
+            netmask_eth0_0: '255.255.255.0',
+            network_eth0_0: '203.0.113.0',
+          )
         end
 
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway   => '127.0.0.254',
-            dhcp_interface => 'eth0.0',
-          }"
+        let(:params) { super().merge(dhcp_interface: 'eth0.0') }
+
+        it do
+          should contain_class('dhcp')
+            .with_dnsdomain(['example.com'])
+            .with_nameservers(['203.0.113.10'])
+            .with_interfaces(['eth0.0'])
+            .with_pxeserver('203.0.113.10')
+            .with_pxefilename('pxelinux.0')
         end
 
-        it do should contain_class('dhcp').with(
-          'dnsdomain'   => ['example.com'],
-          'nameservers' => ['127.0.1.1'],
-          'interfaces'  => ['eth0.0'],
-          'pxeserver'   => '127.0.1.1',
-          'pxefilename' => 'pxelinux.0'
-        ) end
-
-        it do should contain_dhcp__pool('example.com').with(
-          'network'  => '127.0.0.0',
-          'mask'     => '255.0.0.0',
-          'range'    => nil,
-          'gateway'  => '127.0.0.254',
-          'failover' => nil
-        ) end
+        it do
+          should contain_dhcp__pool('example.com')
+            .with_network('203.0.113.0')
+            .with_mask('255.255.255.0')
+            .with_range(nil)
+            .with_gateway('192.0.2.1')
+            .with_failover(nil)
+        end
 
         it { should_not contain_class('dhcp::failover') }
       end
 
       context "on alias interface" do
         let :facts do
-          facts.merge({:ipaddress_eth0_0 => '127.0.1.1',
-                       :netmask_eth0_0   => '255.0.0.0',
-                       :network_eth0_0   => '127.0.0.0'})
+          facts.merge(
+            ipaddress_eth0_0: '198.51.100.10',
+            netmask_eth0_0: '255.255.255.0',
+            network_eth0_0: '198.51.100.0',
+          )
         end
 
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway   => '127.0.0.254',
-            dhcp_interface => 'eth0:0',
-          }"
-        end
+        let(:params) { super().merge(dhcp_interface: 'eth0:0') }
 
-        it do should contain_class('dhcp').with(
-            'dnsdomain'   => ['example.com'],
-            'nameservers' => ['127.0.1.1'],
-            'interfaces'  => ['eth0:0'],
-            'pxeserver'   => '127.0.1.1',
-            'pxefilename' => 'pxelinux.0'
-        ) end
-        it do should contain_dhcp__pool('example.com').with(
-            'network'  => '127.0.0.0',
-            'mask'     => '255.0.0.0',
-            'range'    => nil,
-            'gateway'  => '127.0.0.254',
-            'failover' => nil
-        ) end
+        it do
+          should contain_class('dhcp')
+            .with_dnsdomain(['example.com'])
+            .with_nameservers(['198.51.100.10'])
+            .with_interfaces(['eth0:0'])
+            .with_pxeserver('198.51.100.10')
+            .with_pxefilename('pxelinux.0')
+        end
+        it do
+          should contain_dhcp__pool('example.com')
+            .with_network('198.51.100.0')
+            .with_mask('255.255.255.0')
+            .with_range(nil)
+            .with_gateway('192.0.2.1')
+            .with_failover(nil)
+        end
 
         it { should_not contain_class('dhcp::failover') }
-      end
-
-      context "with additional dhcp listen interfaces" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway => '127.0.0.254',
-            dhcp_additional_interfaces => [ 'vlan8', 'vlan9', 'vlan120' ],
-          }"
-        end
-
-        it do should contain_class('dhcp').with(
-          'dnsdomain'   => ['example.com'],
-          'nameservers' => ['127.0.1.1'],
-          'interfaces'  => ['eth0', 'vlan8', 'vlan9', 'vlan120' ],
-          'pxeserver'   => '127.0.1.1',
-          'pxefilename' => 'pxelinux.0'
-        ) end
-
-        it do should contain_dhcp__pool('example.com').with(
-          'network'  => '127.0.0.0',
-          'mask'     => '255.0.0.0',
-          'range'    => nil,
-          'gateway'  => '127.0.0.254',
-          'failover' => nil
-        ) end
-
-        it { should_not contain_class('dhcp::failover') }
-      end
-
-      context "with one additional dhcp listen interface" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway => '127.0.0.254',
-            dhcp_additional_interfaces => [ 'vlan83' ]
-          }"
-        end
-
-        it do should contain_class('dhcp').with(
-          'dnsdomain'   => ['example.com'],
-          'nameservers' => ['127.0.1.1'],
-          'interfaces'  => ['eth0', 'vlan83'],
-          'pxeserver'   => '127.0.1.1',
-          'pxefilename' => 'pxelinux.0'
-        ) end
-
-        it do should contain_dhcp__pool('example.com').with(
-          'network'  => '127.0.0.0',
-          'mask'     => '255.0.0.0',
-          'range'    => nil,
-          'gateway'  => '127.0.0.254',
-          'failover' => nil
-        ) end
-
-        it { should_not contain_class('dhcp::failover') }
-      end
-
-      context "with additional dhcp listen interfaces wrongly specified as String data type" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway => '127.0.0.254',
-            dhcp_additional_interfaces => 'vlan55',
-          }"
-        end
-        it { should raise_error(Puppet::PreformattedError, /expects an Array value, got String/) }
-      end
-
-      context "with additional dhcp listen interfaces wrongly specified as Hash data type" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway => '127.0.0.254',
-            dhcp_additional_interfaces => { 'name' => 'vlan55' }
-          }"
-        end
-        it { should raise_error(Puppet::PreformattedError, /expects an Array value, got Struct/) }
-      end
-
-      context "with dhcp_search_domains" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_gateway        => '127.0.0.254',
-            dhcp_search_domains => ['example.com', 'example.org']
-          }"
-        end
-
-        it do should contain_dhcp__pool('example.com').with(
-            'search_domains' => ['example.com','example.org']
-        ) end
-      end
-
-      context "with dhcp_pxeserver" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '127.0.1.1',
-                       :netmask_eth0   => '255.0.0.0',
-                       :network_eth0   => '127.0.0.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_pxeserver => '127.0.1.200'
-          }"
-        end
-
-        it do should contain_class('dhcp').with(
-            'pxeserver'   => '127.0.1.200',
-        ) end
-      end
-
-      context "as primary dhcp server" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '192.168.100.20',
-                       :ipaddress      => '192.168.100.20',
-                       :netmask_eth0   => '255.255.255.0',
-                       :network_eth0   => '192.168.100.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_range        => '192.168.100.1 192.168.100.10',
-            dhcp_node_type    => 'primary',
-            dhcp_peer_address => '192.168.1.21',
-          }"
-        end
-
-        it do should contain_class('dhcp::failover').with(
-            'role'         => 'primary',
-            'peer_address' => '192.168.1.21',
-            'address'      => '192.168.100.20'
-        ) end
-
-        it do should contain_class('dhcp').with(
-            'dnsdomain'  => ['example.com'],
-            'interfaces' => ['eth0']
-        ) end
-      end
-
-      context "as secondary dhcp server" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '192.168.100.21',
-                       :ipaddress      => '192.168.100.21',
-                       :netmask_eth0   => '255.255.255.0',
-                       :network_eth0   => '192.168.100.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_range        => '192.168.100.1 192.168.100.10',
-            dhcp_node_type    => 'secondary',
-            dhcp_peer_address => '192.168.1.20',
-          }"
-        end
-
-        it do should contain_class('dhcp::failover').with(
-            'role'         => 'secondary',
-            'peer_address' => '192.168.1.20',
-            'address'      => '192.168.100.21'
-        ) end
-
-        it do should contain_class('dhcp').with(
-            'dnsdomain'  => ['example.com'],
-            'interfaces' => ['eth0']
-        ) end
       end
 
       context "on a non-existing interface" do
-        let :facts do
-          facts
-        end
+        let(:facts) { facts }
+        let(:params) { super().merge(dhcp_interface: 'doesnotexist') }
 
-        let :pre_condition do
-          "class { 'foreman_proxy':
-            dhcp_interface => 'doesnotexist',
-          }"
-        end
-
-        it { should raise_error(Puppet::Error, /Could not get the ip address from fact ipaddress_doesnotexist/) }
-      end
-
-      context "as manager of ACLs for dhcp" unless ['FreeBSD', 'DragonFly'].include?(facts[:osfamily]) do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '192.168.100.20',
-                       :ipaddress      => '192.168.100.20',
-                       :netmask_eth0   => '255.255.255.0',
-                       :network_eth0   => '192.168.100.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy':
-            dhcp_manage_acls  => true,
-          }"
-        end
-
-        it do should contain_exec('Allow foreman-proxy to read /etc/dhcp').
-          with_command("setfacl -R -m u:foreman-proxy:rx /etc/dhcp")
-        end
-
-        it do should contain_exec('Allow foreman-proxy to read /var/lib/dhcpd').
-          with_command("setfacl -R -m u:foreman-proxy:rx /var/lib/dhcpd")
-        end
-      end
-
-      context "as manager of ACLs for dhcp for RedHat only by default" do
-        let :facts do
-          facts.merge({:ipaddress_eth0 => '192.168.100.20',
-                       :ipaddress      => '192.168.100.20',
-                       :netmask_eth0   => '255.255.255.0',
-                       :network_eth0   => '192.168.100.0'})
-        end
-
-        let :pre_condition do
-          "class {'foreman_proxy': }"
-        end
-
-        case facts[:osfamily]
-        when 'RedHat'
-          it do should contain_exec('Allow foreman-proxy to read /etc/dhcp').
-            with_command('setfacl -R -m u:foreman-proxy:rx /etc/dhcp').
-            with_unless('getfacl -p /etc/dhcp | grep user:foreman-proxy:r-x')
-          end
-        else
-          it { should_not contain_exec('Allow foreman-proxy to read /etc/dhcp') }
-        end
-
-        case facts[:osfamily]
-        when 'RedHat'
-          it do should contain_exec('Allow foreman-proxy to read /var/lib/dhcpd').
-            with_command("setfacl -R -m u:foreman-proxy:rx /var/lib/dhcpd").
-            with_unless('getfacl -p /var/lib/dhcpd | grep user:foreman-proxy:r-x')
-          end
-        else
-          it { should_not contain_exec('Allow foreman-proxy to read /var/lib/dhcpd') }
-        end
+        it { should compile.and_raise_error(/Could not get the ip address from fact ipaddress_doesnotexist/) }
       end
     end
   end
