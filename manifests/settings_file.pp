056a0689c11858f267bdaa21d4c441890a8b903a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-03-14 16:29:17 +0100

    Move feature and module definitions to f_p::module
    This moves the definition of foreman_proxy::feature from
    foreman_proxy::settings_file to foreman_proxy::module. It also
    simplified it by only passing a $module_enabled variable. This is still
    needed because it needs to be in context for template().

--- manifests/settings_file.pp
+++ manifests/settings_file.pp
@@ -3,14 +3,8 @@
 # @param ensure
 #   Whether the config file should be a file or absent
 #
-# @param module
-#   Whether the config file is a proxy module or not
-#
-# @param enabled
-#   If module is enabled or not
-#
-# @param listen_on
-#   Whether the module listens on https, http, or both
+# @param module_enabled
+#   If module is enabled or not. Only relevant when it's a module.
 #
 # @param path
 #   Path to module's settings file
@@ -27,43 +21,15 @@
 # @param mode
 #   Settings file's mode
 #
-# @param feature
-#   Feature name advertised by proxy module. If set, foreman_proxy::register
-#   will validate the feature name is loaded and advertised.
-#
 define foreman_proxy::settings_file (
   Enum['file', 'absent'] $ensure = 'file',
-  Boolean $module = true,
-  Boolean $enabled = true,
-  Foreman_proxy::ListenOn $listen_on = 'https',
+  String $module_enabled = 'false',
   Stdlib::Absolutepath $path = "${foreman_proxy::params::config_dir}/settings.d/${title}.yml",
   String $owner = 'root',
   String $group = $foreman_proxy::params::user,
   Stdlib::Filemode $mode = '0640',
   String $template_path = "foreman_proxy/${title}.yml.erb",
-  Optional[String] $feature = undef,
 ) {
-  # If the config file is for a proxy module, then we need to know
-  # whether it's enabled, and if so, where to listen (https, http, or both).
-  # If undefined here, look up the values from the foreman_proxy class.
-
-  if $module {
-    if $enabled {
-      $module_enabled = $listen_on ? {
-        'both'  => true,
-        'https' => 'https',
-        'http'  => 'http',
-        default => false,
-      }
-
-      if $feature and $ensure != 'absent' {
-        foreman_proxy::feature { $feature: }
-      }
-    } else {
-      $module_enabled = false
-    }
-  }
-
   if $ensure == 'absent' {
     $content = undef
   } else {
--- spec/defines/foreman_proxy_settings_file_spec.rb
+++ spec/defines/foreman_proxy_settings_file_spec.rb
@@ -5,6 +5,7 @@ describe 'foreman_proxy::settings_file' do
     context "on #{os}" do
       let(:facts) { facts }
       let(:title) { 'test' }
+      let(:group) { ['FreeBSD', 'DragonFly'].include?(facts[:osfamily]) ? 'foreman_proxy' : 'foreman-proxy' }
       let(:config_path) do
         File.join(
           ['FreeBSD', 'DragonFly'].include?(facts[:osfamily]) ? '/usr/local/etc' : '/etc',
@@ -15,55 +16,14 @@ describe 'foreman_proxy::settings_file' do
       context 'standalone' do
         let(:pre_condition) { 'include foreman_proxy::params' }
 
-        context 'defaults, module enabled' do
-          it do
-            is_expected.to contain_file(config_path).with(
-              ensure: 'file',
-              owner: 'root',
-              group: ['FreeBSD', 'DragonFly'].include?(facts[:osfamily]) ? 'foreman_proxy' : 'foreman-proxy',
-              mode: '0640',
-            )
-          end
-
-          it 'should set :enabled in config' do
-            verify_exact_contents(catalogue, config_path, ['---', ':enabled: https'])
-          end
-
-          it { is_expected.not_to contain_foreman_proxy__feature('Test') }
-        end
-
-        context 'with feature' do
-          let(:params) { { feature: 'Test' } }
-          it { is_expected.to contain_foreman_proxy__feature('Test') }
-        end
-
-        context 'with listen_on => both' do
-          let(:params) { { listen_on: 'both' } }
-
-          it 'should set :enabled to true in config' do
-            verify_exact_contents(catalogue, config_path, ['---', ':enabled: true'])
-          end
-        end
-
-        context 'with listen_on => http' do
-          let(:params) { { listen_on: 'http' } }
-
-          it 'should set :enabled to http in config' do
-            verify_exact_contents(catalogue, config_path, ['---', ':enabled: http'])
-          end
-        end
-
-        context 'with enabled => false' do
-          let(:params) { { enabled: false } }
-
-          it 'should set :enabled to false in config' do
-            verify_exact_contents(catalogue, config_path, ['---', ':enabled: false'])
-          end
-
-          context 'with feature' do
-            let(:params) { { enabled: false, feature: 'Test' } }
-            it { is_expected.not_to contain_foreman_proxy__feature('Test') }
-          end
+        it do
+          is_expected.to compile.with_all_deps
+          is_expected.to contain_file(config_path)
+            .with_ensure('file')
+            .with_owner('root')
+            .with_group(group)
+            .with_mode('0640')
+            .with_content("---\n# Test file only\n# Can be true, false, or http/https to enable just one of the protocols\n:enabled: false\n")
         end
       end
 
