e38d1d9f7a1cf5ea73a605e6622a64ddc03e90ba
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-03-14 21:45:59 +0100

    Remove all OS testing for f_p:feature
    This define contains no OS logic and doesn't need facts so testing on
    every OS is wasting time.

--- spec/defines/foreman_proxy_feature_spec.rb
+++ spec/defines/foreman_proxy_feature_spec.rb
@@ -1,17 +1,13 @@
 require 'spec_helper'
 
 describe 'foreman_proxy::feature' do
-  on_supported_os.each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
-      let(:title) { 'Test' }
+  let(:title) { 'Test' }
 
-      it 'should register feature' do
-        is_expected.to contain_datacat_fragment('foreman_proxy::enabled_features::Test').with(
-          data: { 'features' => ['Test'] },
-          target: 'foreman_proxy::enabled_features',
-        )
-      end
-    end
+  it { is_expected.to compile.with_all_deps }
+  it 'should register feature' do
+    is_expected.to contain_datacat_fragment('foreman_proxy::enabled_features::Test').with(
+      data: { 'features' => ['Test'] },
+      target: 'foreman_proxy::enabled_features',
+    )
   end
 end
