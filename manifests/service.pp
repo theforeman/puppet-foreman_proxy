837f495cff3400269d41e2a6eceb6a3def302861
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-03-15 01:09:16 +0100

    Remove redundant parameters
    The parameter hasstatus has defaulted to true since Puppet 2.7.0 and
    that's long been the minimum supported version.
    
    The require is already ensured in init.pp.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,8 @@
 # @summary Set up the foreman service
 # @api private
 class foreman_proxy::service {
-
   service { 'foreman-proxy':
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-    require   => Class['foreman_proxy::config'],
+    ensure => running,
+    enable => true,
   }
-
 }
