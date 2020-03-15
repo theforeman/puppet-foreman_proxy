f8fbeb610301a4efd2e0554ddb6a1ddf4816e319
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-03-15 17:38:06 +0100

    Use $f_p::plugin::dynflow::external_core
    Since 5bde9c79ca05a7d30fd7ec7a85a109220b4950df there is a variable that
    explicitly sets the external core. Rather than copying the logic, this
    reuses the variable.

--- spec/classes/foreman_proxy__plugin__dynflow_spec.rb
+++ spec/classes/foreman_proxy__plugin__dynflow_spec.rb
@@ -7,7 +7,7 @@ describe 'foreman_proxy::plugin::dynflow' do
       let(:pre_condition) { 'include foreman_proxy' }
       let(:etc_dir) { ['FreeBSD', 'DragonFly'].include?(facts[:osfamily]) ? '/usr/local/etc' : '/etc' }
 
-      has_core = facts[:osfamily] == 'RedHat' && facts[:operatingsystem] != 'Fedora'
+      has_core = facts[:osfamily] == 'RedHat'
 
       describe 'with default settings' do
         it { should compile.with_all_deps }
@@ -69,55 +69,67 @@ describe 'foreman_proxy::plugin::dynflow' do
           :ssl_disabled_ciphers  => ['NULL-MD5', 'NULL-SHA'],
           :tls_disabled_versions => ['1.1'],
           :open_file_limit       => 8000,
-          :external_core         => false,
+          :external_core         => true,
         } end
 
         it { should compile.with_all_deps }
         it { should contain_foreman_proxy__plugin('dynflow') }
 
-        if has_core
-          it 'should create settings.d symlink' do
-            should contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d").
-              with_ensure('link').with_target("#{etc_dir}/foreman-proxy/settings.d")
-          end
+        it 'should create settings.d symlink' do
+          should contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d").
+            with_ensure('link').with_target("#{etc_dir}/foreman-proxy/settings.d")
+        end
 
-          it 'should create systemd service limits' do
-            should contain_systemd__service_limits('smart_proxy_dynflow_core.service').
-              with_limits({'LimitNOFILE' => 8000}).that_notifies('Service[smart_proxy_dynflow_core]')
-          end
+        it 'should create systemd service limits' do
+          should contain_systemd__service_limits('smart_proxy_dynflow_core.service').
+            with_limits({'LimitNOFILE' => 8000}).that_notifies('Service[smart_proxy_dynflow_core]')
+        end
 
-          it 'should generate correct dynflow core settings.yml' do
-            verify_exact_contents(catalogue, "#{etc_dir}/smart_proxy_dynflow_core/settings.yml", [
-              '---',
-              ':database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite',
-              ':console_auth: true',
-              ':foreman_url: https://foo.example.com',
-              ':listen: "*"',
-              ':port: 8008',
-              ':use_https: true',
-              ':ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem',
-              ':ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem',
-              ':ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem',
-              ':ssl_disabled_ciphers: ["NULL-MD5", "NULL-SHA"]',
-              ':tls_disabled_versions: ["1.1"]',
-            ])
-          end
+        it 'should generate correct dynflow core settings.yml' do
+          verify_exact_contents(catalogue, "#{etc_dir}/smart_proxy_dynflow_core/settings.yml", [
+            '---',
+            ':database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite',
+            ':console_auth: true',
+            ':foreman_url: https://foo.example.com',
+            ':listen: "*"',
+            ':port: 8008',
+            ':use_https: true',
+            ':ssl_ca_file: /var/lib/puppet/ssl/certs/ca.pem',
+            ':ssl_certificate: /var/lib/puppet/ssl/certs/foo.example.com.pem',
+            ':ssl_private_key: /var/lib/puppet/ssl/private_keys/foo.example.com.pem',
+            ':ssl_disabled_ciphers: ["NULL-MD5", "NULL-SHA"]',
+            ':tls_disabled_versions: ["1.1"]',
+          ])
+        end
 
-          it 'should generate correct dynflow.yml' do
-            verify_exact_contents(catalogue, "#{etc_dir}/foreman-proxy/settings.d/dynflow.yml", [
-              '---',
-              ':enabled: https',
-              ':database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite',
-              ':core_url: https://foo.example.com:8008',
-              ':external_core: false',
-            ])
-          end
-        else
-          it { should_not contain_foreman_proxy__plugin('dynflow_core') }
-          it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d") }
-          it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.yml") }
-          it { should_not contain_service('smart_proxy_dynflow_core') }
-          it { should_not contain_systemd__service_limits('smart_proxy_dynflow_core.service') }
+        it 'should generate correct dynflow.yml' do
+          verify_exact_contents(catalogue, "#{etc_dir}/foreman-proxy/settings.d/dynflow.yml", [
+            '---',
+            ':enabled: https',
+            ':database: /var/lib/foreman-proxy/dynflow/dynflow.sqlite',
+            ':core_url: https://foo.example.com:8008',
+            ':external_core: true',
+          ])
+        end
+      end
+
+      describe 'without external_core' do
+        let(:params) { { external_core: false } }
+
+        it { should_not contain_foreman_proxy__plugin('dynflow_core') }
+        it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.d") }
+        it { should_not contain_file("#{etc_dir}/smart_proxy_dynflow_core/settings.yml") }
+        it { should_not contain_service('smart_proxy_dynflow_core') }
+        it { should_not contain_systemd__service_limits('smart_proxy_dynflow_core.service') }
+
+        it 'should generate correct dynflow.yml' do
+          verify_exact_contents(catalogue, "#{etc_dir}/foreman-proxy/settings.d/dynflow.yml", [
+            '---',
+            ':enabled: https',
+            ':database: ',
+            ':core_url: https://foo.example.com:8008',
+            ':external_core: false',
+          ])
         end
       end
     end
