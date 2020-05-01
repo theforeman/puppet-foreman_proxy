263f1fc6eabc5126c8bc67ee783827062e6eabb5
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-01 19:40:49 +0200

    Use shared examples to reduce duplication

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -5,20 +5,9 @@ describe 'Scenario: install foreman-proxy' do
 
   include_examples 'the example', 'basic.pp'
 
+  it_behaves_like 'the default foreman proxy application'
+
   describe package('foreman-proxy-journald') do
     it { is_expected.not_to be_installed }
-  end
-
-  describe service('foreman-proxy') do
-    it { is_expected.to be_enabled }
-    it { is_expected.to be_running }
-  end
-
-  describe port(8000) do
-    it { is_expected.not_to be_listening }
-  end
-
-  describe port(8443) do
-    it { is_expected.to be_listening }
   end
 end
--- spec/acceptance/dynflow_spec.rb
+++ spec/acceptance/dynflow_spec.rb
@@ -5,19 +5,7 @@ describe 'Scenario: install foreman-proxy', unless: ENV['BEAKER_HYPERVISOR'] == 
 
   include_examples 'the example', 'dynflow.pp'
 
-  describe service('foreman-proxy') do
-    it { is_expected.to be_enabled }
-    it { is_expected.to be_running }
-  end
-
-  describe port(8000) do
-    it { is_expected.not_to be_listening }
-  end
-
-  describe port(8443) do
-    it { is_expected.to be_listening }
-  end
-
+  it_behaves_like 'the default foreman proxy application'
 
   if os[:family] =~ /redhat|fedora/
     describe service('smart_proxy_dynflow_core') do
