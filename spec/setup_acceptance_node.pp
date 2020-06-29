5a6b51c1a3b343a5a7844fda45622fa5bd74aba0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-06-29 18:00:36 +0200

    Fixes #30240 - compat with theforeman/dns 8.x
    dda99d908f38c3e752928a9a2d570280477d581c marked the module as
    compatible with theforeman/dns 8.x. However, 8.x was a breaking change
    and no longer automatically grants access to the rndc-key by default.
    This restores compatibility.

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -1,3 +1,13 @@
 class { 'foreman::repo':
   repo => 'nightly',
 }
+
+# This provides dig which we use in our tests
+$dig_package = $facts['os']['family'] ? {
+  'Debian' => 'dnsutils',
+  default  => 'bind-utils',
+}
+
+package { $dig_package:
+  ensure => installed,
+}
