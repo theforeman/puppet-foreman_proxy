2a500f4dae38a97b0ffa65de729a13dbfa9ddd18
Eric D. Helms <ericdhelms@gmail.com>
2020-12-16 15:10:41 -0500

    Fixes #32239:  Update Pulp plugin to drop Pulp 2
    Katello 4.0 drops Pulp 2 in favor of Pulp 3 completely. If this
    plugin is used, then Pulpcore will be used and the user can decide
    if it should be deployed as a mirror configuration or not. This drops
    most of the parameters as they are not relevant with Pulp 3.

--- manifests/plugin/pulp/params.pp
+++ manifests/plugin/pulp/params.pp
@@ -1,17 +1,6 @@
 # Default parameters for the Pulp smart proxy plugin
 # @api private
 class foreman_proxy::plugin::pulp::params {
-  $enabled              = true
-  $listen_on            = 'https'
-  $version              = undef
-  $pulpnode_enabled     = false
-  $pulpcore_enabled     = false
-  $pulpcore_mirror      = false
-  $pulp_url             = "https://${facts['networking']['fqdn']}/pulp"
   $pulpcore_api_url     = "https://${facts['networking']['fqdn']}"
-  $pulpcore_content_url = "${pulp_url}/content"
-  $pulp_dir             = '/var/lib/pulp'
-  $pulp_content_dir     = '/var/lib/pulp/content'
-  $puppet_content_dir   = undef
-  $mongodb_dir          = '/var/lib/mongodb'
+  $pulpcore_content_url = "${pulpcore_api_url}/pulp/content"
 }
