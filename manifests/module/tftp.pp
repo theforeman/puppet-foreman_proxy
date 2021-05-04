f70a369ba08e0ae0f45d8e3adb53920ae375810e
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-05-04 12:10:10 +0200

    Lazily load tftp directories
    This change makes tftp directories lazy. It does that by making the
    parameters undef by default and determine them if tftp is actually
    included.
    
    The main motivation is that f849d1284d3ee0a41eae4becc8b1a561b668c93c
    changed the tftp module. Prior to it, the logic was duplicated. With
    that change, it became dynamic which also made it a hard dependency.
    Technically it already was since it's always been listed in
    metadata.json. However, it functioned without it if tftp was false. With
    this change it really becomes optional.

--- manifests/module/tftp.pp
+++ manifests/module/tftp.pp
@@ -5,10 +5,18 @@
 #
 # @param listen_on
 #   Where to listen on.
+#
+# @param tftproot
+#   The root directory to use. This is required if the module is enabled.
 class foreman_proxy::module::tftp (
   Boolean $enabled = $foreman_proxy::tftp,
   Foreman_proxy::ListenOn $listen_on = $foreman_proxy::tftp_listen_on,
+  Optional[Stdlib::Absolutepath] $tftproot = $foreman_proxy::tftp_root,
 ) {
+  if $enabled {
+    assert_type(NotUndef, $tftproot)
+  }
+
   foreman_proxy::module { 'tftp':
     enabled   => $enabled,
     listen_on => $listen_on,
