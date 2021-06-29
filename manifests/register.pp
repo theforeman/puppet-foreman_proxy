1ba0d413969305fa400cfb57671543c3ad73b847
Adam Ruzicka <aruzicka@redhat.com>
2021-06-29 10:28:10 +0200

    Fixes #33790 - Mark host where the installer is running as smart-proxy
    when deploying smart-proxy

--- manifests/register.pp
+++ manifests/register.pp
@@ -12,6 +12,27 @@ class foreman_proxy::register {
       url             => $foreman_proxy::real_registered_proxy_url,
     }
 
+    foreman_host { "foreman-proxy-${$foreman_proxy::registered_name}":
+      ensure          => present,
+      hostname        => $foreman_proxy::registered_name,
+      base_url        => $foreman_proxy::foreman_base_url,
+      consumer_key    => $foreman_proxy::oauth_consumer_key,
+      consumer_secret => $foreman_proxy::oauth_consumer_secret,
+      effective_user  => $foreman_proxy::oauth_effective_user,
+      ssl_ca          => pick($foreman_proxy::foreman_ssl_ca, $foreman_proxy::ssl_ca),
+      facts           => $facts,
+    }
+
+    foreman_smartproxy_host { "foreman-proxy-${$foreman_proxy::registered_name}":
+      ensure          => present,
+      hostname        => $foreman_proxy::registered_name,
+      base_url        => $foreman_proxy::foreman_base_url,
+      consumer_key    => $foreman_proxy::oauth_consumer_key,
+      consumer_secret => $foreman_proxy::oauth_consumer_secret,
+      effective_user  => $foreman_proxy::oauth_effective_user,
+      ssl_ca          => pick($foreman_proxy::foreman_ssl_ca, $foreman_proxy::ssl_ca),
+    }
+
     # Ensure puppet agent is started after registering the Foreman proxy
     # By using collectors, we don't have to test if the collected resource actually exists
     Foreman_smartproxy[$foreman_proxy::registered_name] -> Cron <| title == 'puppet' |>
