theforeman/puppet-foreman_proxy

commit cf0da38cc900f3d927e4de9eed8f4fabc7c5bedc

Move all static vars from params to init
All variables in params.pp that are static are moved to init.pp. This
makes it easier to read init.pp

It also changes some data types from Integer[0, 65535] to Stdlib::Port
which is effectively the same, but makes it easier to read.

manifests/init.pp
@@ -282,137 +282,141 @@
 # $puppetca_certificate::       Token-whitelisting only: Certificate to use when encrypting tokens (undef to use SSL certificate)
 #
 class foreman_proxy (
-  String $version = $foreman_proxy::params::version,
-  Enum['latest', 'present', 'installed', 'absent'] $ensure_packages_version = $foreman_proxy::params::ensure_packages_version,
-  Variant[Array[String], String] $bind_host = $foreman_proxy::params::bind_host,
-  Integer[0, 65535] $http_port = $foreman_proxy::params::http_port,
-  Integer[0, 65535] $ssl_port = $foreman_proxy::params::ssl_port,
-  Array[String] $groups = $foreman_proxy::params::groups,
-  Variant[Enum['STDOUT', 'SYSLOG', 'JOURNAL'], Stdlib::Absolutepath] $log = $foreman_proxy::params::log,
-  Enum['WARN', 'DEBUG', 'ERROR', 'FATAL', 'INFO', 'UNKNOWN'] $log_level = $foreman_proxy::params::log_level,
-  Integer[0] $log_buffer = $foreman_proxy::params::log_buffer,
-  Integer[0] $log_buffer_errors = $foreman_proxy::params::log_buffer_errors,
-  Boolean $http = $foreman_proxy::params::http,
-  Boolean $ssl = $foreman_proxy::params::ssl,
+  String $version = 'present',
+  Enum['latest', 'present', 'installed', 'absent'] $ensure_packages_version = 'present',
+  Variant[Array[String], String] $bind_host = ['*'],
+  Stdlib::Port $http_port = 8000,
+  Stdlib::Port $ssl_port = 8443,
+  Array[String] $groups = [],
+  Variant[Enum['STDOUT', 'SYSLOG', 'JOURNAL'], Stdlib::Absolutepath] $log = '/var/log/foreman-proxy/proxy.log',
+  Enum['WARN', 'DEBUG', 'ERROR', 'FATAL', 'INFO', 'UNKNOWN'] $log_level = 'INFO',
+  Integer[0] $log_buffer = 2000,
+  Integer[0] $log_buffer_errors = 1000,
+  Boolean $http = false,
+  Boolean $ssl = true,
   Stdlib::Absolutepath $ssl_ca = $foreman_proxy::params::ssl_ca,
   Stdlib::Absolutepath $ssl_cert = $foreman_proxy::params::ssl_cert,
   Stdlib::Absolutepath $ssl_key = $foreman_proxy::params::ssl_key,
-  Optional[Stdlib::Absolutepath] $foreman_ssl_ca = $foreman_proxy::params::foreman_ssl_ca,
-  Optional[Stdlib::Absolutepath] $foreman_ssl_cert = $foreman_proxy::params::foreman_ssl_cert,
-  Optional[Stdlib::Absolutepath] $foreman_ssl_key = $foreman_proxy::params::foreman_ssl_key,
+  Optional[Stdlib::Absolutepath] $foreman_ssl_ca = undef,
+  Optional[Stdlib::Absolutepath] $foreman_ssl_cert = undef,
+  Optional[Stdlib::Absolutepath] $foreman_ssl_key = undef,
   Array[String] $trusted_hosts = $foreman_proxy::params::trusted_hosts,
-  Array[String] $ssl_disabled_ciphers = $foreman_proxy::params::ssl_disabled_ciphers,
-  Array[String] $tls_disabled_versions = $foreman_proxy::params::tls_disabled_versions,
-  Boolean $manage_sudoersd = $foreman_proxy::params::manage_sudoersd,
-  Boolean $use_sudoersd = $foreman_proxy::params::use_sudoersd,
-  Boolean $use_sudoers = $foreman_proxy::params::use_sudoers,
-  Boolean $puppetca = $foreman_proxy::params::puppetca,
-  Foreman_proxy::ListenOn $puppetca_listen_on = $foreman_proxy::params::puppetca_listen_on,
+  Array[String] $ssl_disabled_ciphers = [],
+  Array[String] $tls_disabled_versions = [],
+  Boolean $manage_sudoersd = true,
+  Boolean $use_sudoersd = true,
+  Boolean $use_sudoers = true,
+  Boolean $puppetca = true,
+  Foreman_proxy::ListenOn $puppetca_listen_on = 'https',
   Stdlib::Absolutepath $ssldir = $foreman_proxy::params::ssldir,
   Stdlib::Absolutepath $puppetdir = $foreman_proxy::params::puppetdir,
   String $puppetca_cmd = $foreman_proxy::params::puppetca_cmd,
-  String $puppet_group = $foreman_proxy::params::puppet_group,
-  String $puppetca_provider = $foreman_proxy::params::puppetca_provider,
+  String $puppet_group = 'puppet',
+  String $puppetca_provider = 'puppetca_hostname_whitelisting',
   Stdlib::Absolutepath $autosignfile = $foreman_proxy::params::autosignfile,
-  Boolean $puppetca_sign_all = $foreman_proxy::params::puppetca_sign_all,
-  Stdlib::Absolutepath $puppetca_tokens_file = $foreman_proxy::params::puppetca_tokens_file,
-  Integer[0] $puppetca_token_ttl = $foreman_proxy::params::puppetca_token_ttl,
-  Optional[Stdlib::Absolutepath] $puppetca_certificate = $foreman_proxy::params::puppetca_certificate,
-  Boolean $manage_puppet_group = $foreman_proxy::params::manage_puppet_group,
-  Boolean $puppet = $foreman_proxy::params::puppet,
-  Foreman_proxy::ListenOn $puppet_listen_on = $foreman_proxy::params::puppet_listen_on,
+  Boolean $puppetca_sign_all = false,
+  Stdlib::Absolutepath $puppetca_tokens_file = '/var/lib/foreman-proxy/tokens.yml',
+  Integer[0] $puppetca_token_ttl = 360,
+  Optional[Stdlib::Absolutepath] $puppetca_certificate = undef,
+  Boolean $manage_puppet_group = true,
+  Boolean $puppet = true,
+  Foreman_proxy::ListenOn $puppet_listen_on = 'https',
   Stdlib::HTTPUrl $puppet_url = $foreman_proxy::params::puppet_url,
   Stdlib::Absolutepath $puppet_ssl_ca = $foreman_proxy::params::ssl_ca,
   Stdlib::Absolutepath $puppet_ssl_cert = $foreman_proxy::params::ssl_cert,
   Stdlib::Absolutepath $puppet_ssl_key = $foreman_proxy::params::ssl_key,
-  Integer[0] $puppet_api_timeout = $foreman_proxy::params::puppet_api_timeout,
-  Boolean $templates = $foreman_proxy::params::templates,
-  Foreman_proxy::ListenOn $templates_listen_on = $foreman_proxy::params::templates_listen_on,
+  Integer[0] $puppet_api_timeout = 30,
+  Boolean $templates = false,
+  Foreman_proxy::ListenOn $templates_listen_on = 'https',
   Stdlib::HTTPUrl $template_url = $foreman_proxy::params::template_url,
-  Boolean $registration = $foreman_proxy::params::registration,
-  Foreman_proxy::ListenOn $registration_listen_on = $foreman_proxy::params::registration_listen_on,
-  Boolean $logs = $foreman_proxy::params::logs,
-  Foreman_proxy::ListenOn $logs_listen_on = $foreman_proxy::params::logs_listen_on,
-  Optional[Boolean] $httpboot = $foreman_proxy::params::httpboot,
-  Foreman_proxy::ListenOn $httpboot_listen_on = $foreman_proxy::params::httpboot_listen_on,
-  Boolean $tftp = $foreman_proxy::params::tftp,
-  Foreman_proxy::ListenOn $tftp_listen_on = $foreman_proxy::params::tftp_listen_on,
-  Boolean $tftp_managed = $foreman_proxy::params::tftp_managed,
-  Boolean $tftp_manage_wget = $foreman_proxy::params::tftp_manage_wget,
+  Boolean $registration = true,
+  Foreman_proxy::ListenOn $registration_listen_on = 'https',
+  Boolean $logs = true,
+  Foreman_proxy::ListenOn $logs_listen_on = 'https',
+  Boolean $httpboot = false,
+  Foreman_proxy::ListenOn $httpboot_listen_on = 'both',
+  Boolean $tftp = false,
+  Foreman_proxy::ListenOn $tftp_listen_on = 'https',
+  Boolean $tftp_managed = true,
+  Boolean $tftp_manage_wget = true,
   Array[Stdlib::Absolutepath] $tftp_syslinux_filenames = $foreman_proxy::params::tftp_syslinux_filenames,
   Optional[Stdlib::Absolutepath] $tftp_root = $foreman_proxy::params::tftp_root,
-  Optional[Array[Stdlib::Absolutepath]] $tftp_dirs = $foreman_proxy::params::tftp_dirs,
-  Optional[String] $tftp_servername = $foreman_proxy::params::tftp_servername,
-  Boolean $tftp_replace_grub2_cfg = $foreman_proxy::params::tftp_replace_grub2_cfg,
-  Boolean $dhcp = $foreman_proxy::params::dhcp,
-  Foreman_proxy::ListenOn $dhcp_listen_on = $foreman_proxy::params::dhcp_listen_on,
-  Boolean $dhcp_managed = $foreman_proxy::params::dhcp_managed,
-  String $dhcp_provider = $foreman_proxy::params::dhcp_provider,
-  Array[String] $dhcp_subnets = $foreman_proxy::params::dhcp_subnets,
-  Boolean $dhcp_ping_free_ip = $foreman_proxy::params::dhcp_ping_free_ip,
+  Optional[Array[Stdlib::Absolutepath]] $tftp_dirs = undef,
+  Optional[String] $tftp_servername = undef,
+  Boolean $tftp_replace_grub2_cfg = false,
+  Boolean $dhcp = false,
+  Foreman_proxy::ListenOn $dhcp_listen_on = 'https',
+  Boolean $dhcp_managed = true,
+  String $dhcp_provider = 'isc',
+  Array[String] $dhcp_subnets = [],
+  Boolean $dhcp_ping_free_ip = true,
   Array[String] $dhcp_option_domain = $foreman_proxy::params::dhcp_option_domain,
-  Optional[Array[String]] $dhcp_search_domains = $foreman_proxy::params::dhcp_search_domains,
+  Optional[Array[String]] $dhcp_search_domains = undef,
   String $dhcp_interface = $foreman_proxy::params::dhcp_interface,
-  Array[String] $dhcp_additional_interfaces = $foreman_proxy::params::dhcp_additional_interfaces,
-  Optional[String] $dhcp_gateway = $foreman_proxy::params::dhcp_gateway,
-  Variant[Undef, Boolean, String] $dhcp_range = $foreman_proxy::params::dhcp_range,
-  Optional[String] $dhcp_pxeserver = $foreman_proxy::params::dhcp_pxeserver,
-  String $dhcp_pxefilename = $foreman_proxy::params::dhcp_pxefilename,
-  Optional[String] $dhcp_network = $foreman_proxy::params::dhcp_network,
-  Optional[String] $dhcp_netmask = $foreman_proxy::params::dhcp_netmask,
-  String $dhcp_nameservers = $foreman_proxy::params::dhcp_nameservers,
-  String $dhcp_server = $foreman_proxy::params::dhcp_server,
+  Array[String] $dhcp_additional_interfaces = [],
+  Optional[String] $dhcp_gateway = undef,
+  Variant[Undef, Boolean, String] $dhcp_range = undef,
+  Optional[String] $dhcp_pxeserver = undef,
+  String $dhcp_pxefilename = 'pxelinux.0',
+  Optional[String] $dhcp_network = undef,
+  Optional[String] $dhcp_netmask = undef,
+  String $dhcp_nameservers = 'default',
+  String $dhcp_server = '127.0.0.1',
   Stdlib::Absolutepath $dhcp_config = $foreman_proxy::params::dhcp_config,
   Stdlib::Absolutepath $dhcp_leases = $foreman_proxy::params::dhcp_leases,
-  Optional[String] $dhcp_key_name = $foreman_proxy::params::dhcp_key_name,
-  Optional[String] $dhcp_key_secret = $foreman_proxy::params::dhcp_key_secret,
-  Integer[0, 65535] $dhcp_omapi_port = $foreman_proxy::params::dhcp_omapi_port,
-  Optional[String] $dhcp_peer_address = $foreman_proxy::params::dhcp_peer_address,
-  Enum['standalone', 'primary', 'secondary'] $dhcp_node_type = $foreman_proxy::params::dhcp_node_type,
+  Optional[String] $dhcp_key_name = undef,
+  Optional[String] $dhcp_key_secret = undef,
+  Stdlib::Port $dhcp_omapi_port = 7911,
+  Optional[String] $dhcp_peer_address = undef,
+  Enum['standalone', 'primary', 'secondary'] $dhcp_node_type = 'standalone',
   Optional[String] $dhcp_failover_address = $foreman_proxy::params::dhcp_failover_address,
-  Optional[Integer[0, 65535]] $dhcp_failover_port = $foreman_proxy::params::dhcp_failover_port,
-  Optional[Integer[0]] $dhcp_max_response_delay = $foreman_proxy::params::dhcp_max_response_delay,
-  Optional[Integer[0]] $dhcp_max_unacked_updates = $foreman_proxy::params::dhcp_max_unacked_updates,
-  Optional[Integer[0]] $dhcp_mclt = $foreman_proxy::params::dhcp_mclt,
-  Optional[Integer[0, 255]] $dhcp_load_split = $foreman_proxy::params::dhcp_load_split,
-  Optional[Integer[0]] $dhcp_load_balance = $foreman_proxy::params::dhcp_load_balance,
+  Optional[Stdlib::Port] $dhcp_failover_port = 519,
+  Optional[Integer[0]] $dhcp_max_response_delay = 30,
+  Optional[Integer[0]] $dhcp_max_unacked_updates = 10,
+  Optional[Integer[0]] $dhcp_mclt = 300,
+  Optional[Integer[0, 255]] $dhcp_load_split = 255,
+  Optional[Integer[0]] $dhcp_load_balance = 3,
   Boolean $dhcp_manage_acls = $foreman_proxy::params::dhcp_manage_acls,
-  Boolean $dns = $foreman_proxy::params::dns,
-  Foreman_proxy::ListenOn $dns_listen_on = $foreman_proxy::params::dns_listen_on,
-  Boolean $dns_managed = $foreman_proxy::params::dns_managed,
-  String $dns_provider = $foreman_proxy::params::dns_provider,
+  Boolean $dns = false,
+  Foreman_proxy::ListenOn $dns_listen_on = 'https',
+  Boolean $dns_managed = true,
+  String $dns_provider = 'nsupdate',
   String $dns_interface = $foreman_proxy::params::dns_interface,
   Optional[Stdlib::Fqdn] $dns_zone = $foreman_proxy::params::dns_zone,
-  Optional[Variant[String, Array[String]]] $dns_reverse = $foreman_proxy::params::dns_reverse,
-  String $dns_server = $foreman_proxy::params::dns_server,
-  Integer[0] $dns_ttl = $foreman_proxy::params::dns_ttl,
+  Optional[Variant[String, Array[String]]] $dns_reverse = undef,
+  String $dns_server = '127.0.0.1',
+  Integer[0] $dns_ttl = 86400,
   String $dns_tsig_keytab = $foreman_proxy::params::dns_tsig_keytab,
   String $dns_tsig_principal = $foreman_proxy::params::dns_tsig_principal,
-  Array[String] $dns_forwarders = $foreman_proxy::params::dns_forwarders,
-  String $libvirt_network = $foreman_proxy::params::libvirt_network,
-  String $libvirt_connection = $foreman_proxy::params::libvirt_connection,
-  Boolean $bmc = $foreman_proxy::params::bmc,
-  Foreman_proxy::ListenOn $bmc_listen_on = $foreman_proxy::params::bmc_listen_on,
-  Enum['ipmitool', 'freeipmi', 'shell', 'ssh'] $bmc_default_provider = $foreman_proxy::params::bmc_default_provider,
-  String $bmc_ssh_user = $foreman_proxy::params::bmc_ssh_user,
-  Stdlib::Absolutepath $bmc_ssh_key = $foreman_proxy::params::bmc_ssh_key,
-  String $bmc_ssh_powerstatus = $foreman_proxy::params::bmc_ssh_powerstatus,
-  String $bmc_ssh_powercycle = $foreman_proxy::params::bmc_ssh_powercycle,
-  String $bmc_ssh_poweroff = $foreman_proxy::params::bmc_ssh_poweroff,
-  String $bmc_ssh_poweron = $foreman_proxy::params::bmc_ssh_poweron,
-  Boolean $realm = $foreman_proxy::params::realm,
-  Foreman_proxy::ListenOn $realm_listen_on = $foreman_proxy::params::realm_listen_on,
-  String $realm_provider = $foreman_proxy::params::realm_provider,
+  Array[String] $dns_forwarders = [],
+  String $libvirt_network = 'default',
+  String $libvirt_connection = 'qemu:///system',
+  Boolean $bmc = false,
+  Foreman_proxy::ListenOn $bmc_listen_on = 'https',
+  Enum['ipmitool', 'freeipmi', 'shell', 'ssh'] $bmc_default_provider = 'ipmitool',
+  String $bmc_ssh_user = 'root',
+  Stdlib::Absolutepath $bmc_ssh_key = '/usr/share/foreman/.ssh/id_rsa',
+  # lint:ignore:quoted_booleans
+  String $bmc_ssh_powerstatus = 'true',
+  # lint:endignore
+  String $bmc_ssh_powercycle = 'shutdown -r +1',
+  String $bmc_ssh_poweroff = 'shutdown +1',
+  # lint:ignore:quoted_booleans
+  String $bmc_ssh_poweron = 'false',
+  # lint:endignore
+  Boolean $realm = false,
+  Foreman_proxy::ListenOn $realm_listen_on = 'https',
+  String $realm_provider = 'freeipa',
   Stdlib::Absolutepath $realm_keytab = $foreman_proxy::params::realm_keytab,
   String $realm_principal = $foreman_proxy::params::realm_principal,
-  Stdlib::Absolutepath $freeipa_config = $foreman_proxy::params::freeipa_config,
-  Boolean $freeipa_remove_dns = $foreman_proxy::params::freeipa_remove_dns,
+  Stdlib::Absolutepath $freeipa_config = '/etc/ipa/default.conf',
+  Boolean $freeipa_remove_dns = true,
   Variant[Undef, String[0], Stdlib::Absolutepath] $keyfile = $foreman_proxy::params::keyfile,
-  Boolean $register_in_foreman = $foreman_proxy::params::register_in_foreman,
+  Boolean $register_in_foreman = true,
   Stdlib::HTTPUrl $foreman_base_url = $foreman_proxy::params::foreman_base_url,
   String $registered_name = $foreman_proxy::params::registered_name,
-  Optional[Stdlib::HTTPUrl] $registered_proxy_url = $foreman_proxy::params::registered_proxy_url,
-  String $oauth_effective_user = $foreman_proxy::params::oauth_effective_user,
+  Optional[Stdlib::HTTPUrl] $registered_proxy_url = undef,
+  String $oauth_effective_user = 'admin',
   String $oauth_consumer_key = $foreman_proxy::params::oauth_consumer_key,
   String $oauth_consumer_secret = $foreman_proxy::params::oauth_consumer_secret,
 ) inherits foreman_proxy::params {

manifests/params.pp
@@ -130,200 +130,54 @@ class foreman_proxy::params inherits foreman_proxy::globals {
 
   $puppet_cmd = "${puppet_bindir}/puppet"
 
-  $groups = []
-
-  # Packaging
-  $repo                    = undef
-  $gpgcheck                = true
-  $version                 = 'present'
-  $ensure_packages_version = 'present'
-
-  # Enable listening on http
-  $bind_host = ['*']
-  $http      = false
-  $http_port = 8000
-
-  # Logging settings
-  $log               = '/var/log/foreman-proxy/proxy.log'
-  $log_level         = 'INFO'
-  $log_buffer        = 2000
-  $log_buffer_errors = 1000
-
-  # Enable SSL, ensure proxy is added with "https://" protocol if true
-  $ssl      = true
-  $ssl_port = 8443
   # If CA is specified, remote Foreman host will be verified
   $ssl_ca = "${ssldir}/certs/ca.pem"
   # Used to communicate to Foreman
   $ssl_cert = "${ssldir}/certs/${lower_fqdn}.pem"
   $ssl_key = "${ssldir}/private_keys/${lower_fqdn}.pem"
-  $ssl_disabled_ciphers = []
-  $tls_disabled_versions = []
-
-  $foreman_ssl_ca  = undef
-  $foreman_ssl_cert = undef
-  $foreman_ssl_key  = undef
 
   # Only hosts listed will be permitted, empty array to disable authorization
   $trusted_hosts = [$lower_fqdn]
 
   $sudoers = "${etc}/sudoers"
 
-  # Whether to manage File["$etc/sudoers.d"] or not.  When reusing this module,
-  # this may be disabled to let a dedicated sudo module manage it instead.
-  $manage_sudoersd = true
-
-  # Setting both $use_sudoersd and $use_sudoers to false means this module will not
-  # touch any sudoers entries. Setting both to true will result in sudoersd winning.
-  # Add a file to /etc/sudoers.d (true).
-  $use_sudoersd = true
-
-  # Add contents to /etc/sudoers (true, only if $use_sudoers is false).
-  $use_sudoers = true
-
   # puppet settings
-  $puppet_url                 = "https://${facts['networking']['fqdn']}:8140"
-  $puppet_api_timeout         = 30
+  $puppet_url = "https://${facts['networking']['fqdn']}:8140"
 
   # puppetca settings
-  $puppetca              = true
-  $puppetca_provider     = 'puppetca_hostname_whitelisting'
-  $puppetca_listen_on    = 'https'
-  $puppetca_cmd          = "${puppet_cmd} cert"
-  $puppet_group          = 'puppet'
-  $autosignfile          = "${puppetdir}/autosign.conf"
-  $puppetca_sign_all     = false
-  $puppetca_tokens_file  = '/var/lib/foreman-proxy/tokens.yml'
-  $puppetca_token_ttl    = 360
-  $puppetca_certificate  = undef
-
-  # The puppet-agent package, (puppet 4 AIO) doesn't create a puppet group
-  $manage_puppet_group = true
-
-  # puppetrun settings
-  $puppet = true
-  $puppet_listen_on = 'https'
+  $puppetca_cmd = "${puppet_cmd} cert"
+  $autosignfile = "${puppetdir}/autosign.conf"
 
   # Template settings
-  $templates           = false
-  $templates_listen_on = 'both'
-  $template_url        = "http://${facts['networking']['fqdn']}:${http_port}"
-
-  # Registration settings
-  $registration           = true
-  $registration_listen_on = 'https'
-
-  # Logs settings
-  $logs           = true
-  $logs_listen_on = 'https'
-
-  # HTTPBoot settings
-  $httpboot           = false
-  $httpboot_listen_on = 'both'
+  $template_url = "http://${facts['networking']['fqdn']}:8000"
 
   # TFTP settings - requires optional TFTP puppet module
-  $tftp                   = false
-  $tftp_listen_on         = 'https'
-  $tftp_managed           = true
-  $tftp_manage_wget       = true
-  $tftp_root              = lookup('tftp::root', undef, undef, undef)
-  $tftp_dirs              = undef
-  $tftp_servername        = undef
-  $tftp_replace_grub2_cfg = false
+  $tftp_root = lookup('tftp::root', undef, undef, undef)
 
   # DHCP settings - requires optional DHCP puppet module
-  $dhcp                    = false
-  $dhcp_listen_on          = 'https'
-  $dhcp_managed            = true
-  $dhcp_provider           = 'isc'
-  $dhcp_subnets            = []
-  $dhcp_ping_free_ip       = true
-  $dhcp_interface          = pick(fact('networking.primary'), 'eth0')
-  $dhcp_additional_interfaces = []
-  $dhcp_gateway            = undef
-  $dhcp_range              = undef
-  $dhcp_option_domain      = [$facts['networking']['domain']]
-  $dhcp_search_domains     = undef
-  $dhcp_pxeserver          = undef
-  $dhcp_pxefilename        = 'pxelinux.0'
-  $dhcp_network            = undef
-  $dhcp_netmask            = undef
-  # This will use the IP of the interface in $dhcp_interface, override
-  # if you need to. You can make this a comma-separated string too - it
-  # will be split into an array
-  $dhcp_nameservers = 'default'
-  $dhcp_server      = '127.0.0.1'
-  # Omapi settings
-  $dhcp_key_name   = undef
-  $dhcp_key_secret = undef
-  $dhcp_omapi_port = 7911
-  $dhcp_node_type = 'standalone'
+  $dhcp_interface        = pick(fact('networking.primary'), 'eth0')
+  $dhcp_option_domain    = [$facts['networking']['domain']]
   $dhcp_failover_address = fact('ipaddress')
-  $dhcp_peer_address = undef
-  $dhcp_failover_port = 519
-  $dhcp_max_response_delay = 30
-  $dhcp_max_unacked_updates  = 10
-  $dhcp_mclt  = 300
-  $dhcp_load_split = 255
-  $dhcp_load_balance = 3
 
   # DNS settings - requires optional DNS puppet module
-  $dns                    = false
-  $dns_listen_on          = 'https'
-  $dns_managed            = true
-  $dns_provider           = 'nsupdate'
-  $dns_interface          = pick(fact('networking.primary'), 'eth0')
-  $dns_zone               = $facts['networking']['domain']
+  $dns_interface      = pick(fact('networking.primary'), 'eth0')
+  $dns_zone           = $facts['networking']['domain']
   if $dns_zone {
-    $dns_realm            = upcase($dns_zone)
+    $dns_realm        = upcase($dns_zone)
   } else {
-    $dns_realm            = undef
+    $dns_realm        = undef
   }
-  $dns_reverse            = undef
-  # localhost can resolve to ipv6 which ruby doesn't handle well
-  $dns_server             = '127.0.0.1'
-  $dns_ttl                = 86400
-  $dns_tsig_keytab        = "${config_dir}/dns.keytab"
-  $dns_tsig_principal     = "foremanproxy/${facts['networking']['fqdn']}@${dns_realm}"
-
-  $dns_forwarders = []
-
-  # libvirt options
-  $libvirt_connection = 'qemu:///system'
-  $libvirt_network    = 'default'
-
-  # BMC options
-  $bmc                  = false
-  $bmc_listen_on        = 'https'
-  $bmc_default_provider = 'ipmitool'
-  $bmc_ssh_user         = 'root'
-  $bmc_ssh_key          = '/usr/share/foreman/.ssh/id_rsa'
-  $bmc_ssh_powercycle   = 'shutdown -r +1'
-  $bmc_ssh_poweroff     = 'shutdown +1'
-  # lint:ignore:quoted_booleans
-  $bmc_ssh_poweron      = 'false'
-  $bmc_ssh_powerstatus  = 'true'
-  # lint:endignore
+  $dns_tsig_keytab    = "${config_dir}/dns.keytab"
+  $dns_tsig_principal = "foremanproxy/${facts['networking']['fqdn']}@${dns_realm}"
 
   # Realm management options
-  $realm              = false
-  $realm_listen_on    = 'https'
-  $realm_provider     = 'freeipa'
-  $realm_keytab       = "${config_dir}/freeipa.keytab"
-  $realm_principal    = 'realm-proxy@EXAMPLE.COM'
-  $freeipa_config     = '/etc/ipa/default.conf'
-  $freeipa_remove_dns = true
+  $realm_keytab    = "${config_dir}/freeipa.keytab"
+  $realm_principal = "realm-proxy@${dns_realm}"
 
-  # Proxy can register itself within a Foreman instance
-  $register_in_foreman = true
   # Foreman instance URL for registration
   $foreman_base_url = "https://${lower_fqdn}"
   # Name the proxy should be registered with
   $registered_name = $facts['networking']['fqdn']
-  # Proxy URL to be registered
-  $registered_proxy_url = undef
-  # User to be used for registration
-  $oauth_effective_user = 'admin'
   # OAuth credentials
   # shares cached_data with the foreman module so they're the same
   $oauth_consumer_key    = extlib::cache_data('foreman_cache_data', 'oauth_consumer_key', extlib::random_password(32))

commit e9e7d45662ad09569f8e55971ac11db8a188f802

Fixes #33808 - Make templates listen on both again
In cf0da38cc900f3d927e4de9eed8f4fabc7c5bedc the parameters were changed
and this unintentionally introduced a regression by changing the default
from both to https.

Fixes: cf0da38cc900f3d927e4de9eed8f4fabc7c5bedc

manifests/init.pp
@@ -327,7 +327,7 @@ class foreman_proxy (
   Stdlib::Absolutepath $puppet_ssl_key = $foreman_proxy::params::ssl_key,
   Integer[0] $puppet_api_timeout = 30,
   Boolean $templates = false,
-  Foreman_proxy::ListenOn $templates_listen_on = 'https',
+  Foreman_proxy::ListenOn $templates_listen_on = 'both',
   Stdlib::HTTPUrl $template_url = $foreman_proxy::params::template_url,
   Boolean $registration = true,
   Foreman_proxy::ListenOn $registration_listen_on = 'https',