efa5c92b0d3b4ae34344e788cbe9268ce671b576
Eric D. Helms <ericdhelms@gmail.com>
2022-04-12 23:00:10 -0400

    Add acceptance tests for remote_execution::ssh

---
+++ spec/acceptance/remote_execution_ssh_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper_acceptance'
+
+describe 'Scenario: install foreman-proxy with remote_execution ssh plugin'  do
+  before(:context) { purge_installed_packages }
+
+  include_examples 'the example', 'remote_execution_ssh.pp'
+
+  it_behaves_like 'the default foreman proxy application'
+end
