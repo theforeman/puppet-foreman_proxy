321002efd8994ed397e57fcce514c60fc816bb3a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-04-14 10:21:06 +0200

    Update spec/acceptance/remote_execution_ssh_spec.rb

--- spec/acceptance/remote_execution_ssh_spec.rb
+++ spec/acceptance/remote_execution_ssh_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper_acceptance'
 
-describe 'Scenario: install foreman-proxy with remote_execution ssh plugin'  do
+describe 'Scenario: install foreman-proxy with remote_execution ssh plugin' do
   before(:context) { purge_installed_packages }
 
   include_examples 'the example', 'remote_execution_ssh.pp'
