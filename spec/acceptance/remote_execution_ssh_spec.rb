ece67ac69a5720eb2bda208ee0345420b89df266
Joshua Hoblitt <josh@hoblitt.com>
2022-04-18 16:15:19 -0700

    add foreman_proxy::plugin::remote_execution::ssh::ssh_log_level param

--- spec/acceptance/remote_execution_ssh_spec.rb
+++ spec/acceptance/remote_execution_ssh_spec.rb
@@ -6,4 +6,20 @@ describe 'Scenario: install foreman-proxy with remote_execution ssh plugin' do
   include_examples 'the example', 'remote_execution_ssh.pp'
 
   it_behaves_like 'the default foreman proxy application'
+
+  describe file('/etc/foreman-proxy/settings.d/remote_execution_ssh.yml') do
+    its(:content) { is_expected.to_not match %r{:ssh_log_level:} }
+  end
+end
+
+describe 'Scenario: install foreman-proxy with remote_execution ssh plugin and ssh_log_level param' do
+  before(:context) { purge_installed_packages }
+
+  include_examples 'the example', 'remote_execution_ssh-ssh_log_level.pp'
+
+  it_behaves_like 'the default foreman proxy application'
+
+  describe file('/etc/foreman-proxy/settings.d/remote_execution_ssh.yml') do
+    its(:content) { is_expected.to match %r{:ssh_log_level: debug} }
+  end
 end
