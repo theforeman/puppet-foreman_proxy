156c30dbd1822e6e916398891143fefe55419942
chr1s692 <christian-sam@gmx.de>
2023-07-05 13:45:23 +0200

    Update manifests/service.pp
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,7 @@
 # @summary Set up the foreman service
 # @api private
 class foreman_proxy::service {
-  if $foreman_proxy::manage_service == true
+  if $foreman_proxy::manage_service {
     service { 'foreman-proxy':
       ensure => running,
       enable => true,
