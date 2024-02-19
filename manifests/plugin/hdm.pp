3c59385e497bfb8948a477de6c92da9643e5196d
Martin Alfke <tuxmea@users.noreply.github.com>
2024-02-19 19:25:22 +0100

    Add HDM smart proxy plugin
    The HDM Smart Proxy plugins needs a configuration file, which specifies:
    - the HDM URL
    - optional HDM user and password
    
    User and password can be omitted, if HDM runs without authentication.

---
+++ manifests/plugin/hdm.pp
@@ -0,0 +1,37 @@
+# = Foreman Proxy HDM plugin
+#
+# This class installs the HDM plugin
+#
+# === Parameters:
+#
+# $hdm_url::                  the API endpoint of your HDM installation
+#
+# $hdm_user::                 email address of the API-role user inside of HDM - can be ommited if HDM runs unauthenticated
+#
+# $hdm_password::             password of the HDM API user - can be ommited if HDM runs unauthenticated
+#
+# === Advanced parameters:
+#
+# $enabled::                  enables/disables the HDM plugin
+#
+# $listen_on::                proxy feature listens on http, https, or both
+#
+# $version::                  plugin package version, it's passed to ensure parameter of package resource
+#                             can be set to specific version number, 'latest', 'present' etc.
+#
+class foreman_proxy::plugin::hdm (
+  String[1]               $hdm_url,
+  Optional[String]        $version      = undef,
+  Boolean                 $enabled      = true,
+  Foreman_proxy::ListenOn $listen_on    = 'https',
+  Optional[String[1]]     $hdm_user     = undef,
+  Optional[String[1]]     $hdm_password = undef,
+) {
+  foreman_proxy::plugin::module { 'hdm':
+    template_path => 'foreman_proxy/plugin/hdm.yml.erb',
+    enabled       => $enabled,
+    feature       => 'HDM',
+    listen_on     => $listen_on,
+    version       => $version,
+  }
+}
