f0f3277477ad693a4e7e73a42c97c1521d9cae54
Martin Alfke <ma@betadots.de>
2024-02-19 18:07:13 +0100

    add parameter documentation

--- manifests/plugin/hdm.pp
+++ manifests/plugin/hdm.pp
@@ -6,12 +6,18 @@
 #
 # === Advanced parameters:
 #
-# $enabled::                  enables/disables the acd plugin
+# $hdm_url::                  the API endpoint of your HDM installation
+#
+# $enabled::                  enables/disables the HDM plugin
 #
 # $listen_on::                proxy feature listens on http, https, or both
 #
 # $version::                  plugin package version, it's passed to ensure parameter of package resource
 #                             can be set to specific version number, 'latest', 'present' etc.
+#
+# $hdm_user::                 email address of the API-role user inside of HDM - can be ommited if HDM runs unauthenticated
+#
+# $hdm_password::             password of the HDM API use - can be ommited if HDM runs unauthenticatedr
 #
 class foreman_proxy::plugin::hdm (
   String[1]               $hdm_url,
