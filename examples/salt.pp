000a3ed35c98975e3878e29792e354ebcfbed46c
Evgeni Golov <evgeni@golov.de>
2024-11-13 09:57:03 +0100

    use new Broadcom repo in Salt examples

--- examples/salt.pp
+++ examples/salt.pp
@@ -1,9 +1,7 @@
-$baseurl = "https://repo.saltproject.io/salt/py3/redhat/${facts['os']['release']['major']}/\$basearch/latest"
-
 yumrepo { 'salt-repo':
-  descr   => "Salt repo for RHEL/CentOS ${facts['os']['release']['major']} PY3",
-  baseurl => $baseurl,
-  gpgkey  => "${baseurl}/SALT-PROJECT-GPG-PUBKEY-2023.pub",
+  descr   => 'Salt repo for RHEL/CentOS',
+  baseurl => 'https://packages.broadcom.com/artifactory/saltproject-rpm/',
+  gpgkey  => 'https://packages.broadcom.com/artifactory/api/security/keypair/SaltProjectKey/public',
   before  => Class['foreman_proxy::plugin::salt'],
 }
 
