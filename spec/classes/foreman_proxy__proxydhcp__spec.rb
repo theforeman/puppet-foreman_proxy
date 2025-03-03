theforeman/puppet-foreman_proxy

commit f8fb58e2ca062dd9ba859de86edcc00696a4958d

Fixes #30632 - Set up HTTPClients in DHCP

manifests/proxydhcp.pp
@@ -82,4 +82,16 @@ class foreman_proxy::proxydhcp {
       omapi_key           => $foreman_proxy::dhcp_key_secret,
     }
   }
+
+  if $foreman_proxy::httpboot {
+    if $foreman_proxy::http {
+      $proxy_base_url = "http://${ip}:${foreman_proxy::http_port}"
+    } else {
+      $proxy_base_url = "https://${ip}:${foreman_proxy::https_port}"
+    }
+
+    dhcp::dhcp_class { 'httpclients':
+      parameters => template('foreman_proxy/httpboot_dhcp_class.erb'),
+    }
+  }
 }

examples/httpboot.pp
@@ -1,0 +1,37 @@
+$directory = '/etc/foreman-proxy'
+$certificate = "${directory}/certificate.pem"
+$key = "${directory}/key.pem"
+
+# Install a proxy with HTTPBoot
+# Note DHCP is not strictly needed, but this is enabled to test as much
+# functionality as possible.
+class { 'foreman_proxy':
+  http                => true,
+  httpboot            => true,
+  tftp                => true,
+  dhcp                => true,
+  puppet_group        => 'root',
+  register_in_foreman => false,
+  ssl_ca              => $certificate,
+  ssl_cert            => $certificate,
+  ssl_key             => $key,
+}
+
+# Create the certificates - this is after the proxy because we need the user variable
+exec { 'Create certificate directory':
+  command => "mkdir -p ${directory}",
+  path    => ['/bin', '/usr/bin'],
+  creates => $directory,
+}
+-> exec { 'Generate certificate':
+  command => "openssl req -nodes -x509 -newkey rsa:2048 -subj '/CN=${facts['networking']['fqdn']}' -keyout '${key}' -out '${certificate}' -days 365",
+  path    => ['/bin', '/usr/bin'],
+  creates => $certificate,
+  umask   => '0022',
+}
+-> file { [$key, $certificate]:
+  owner  => $foreman_proxy::user,
+  group  => $foreman_proxy::user,
+  mode   => '0640',
+  before => Class['foreman_proxy::service'],
+}

spec/acceptance/httpboot_spec.rb
@@ -1,0 +1,29 @@
+require 'spec_helper_acceptance'
+
+describe 'Scenario: install foreman-proxy with HTTPBoot' do
+  include_examples 'the example', 'httpboot.pp'
+
+  describe service('foreman-proxy') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe port(8000) do
+    it { is_expected.to be_listening }
+  end
+
+  describe port(8443) do
+    it { is_expected.to be_listening }
+  end
+
+  describe command('curl --silent --write-out \'%{http_code}\' --output /dev/null http://localhost:8000/EFI/grub2/shim.efi') do
+    its(:stdout) { should eq('200') }
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe file('/etc/dhcp/dhcpd.conf') do
+    it { should be_file }
+    its(:content) { should match(/class "httpclients" {/) }
+    its(:content) { should match(%r{filename "http://\d+\.\d+\.\d+\.\d+:8000/EFI/grub2/shim.efi";}) }
+  end
+end

templates/httpboot_dhcp_class.erb
@@ -1,0 +1,7 @@
+match if substring(option vendor-class-identifier, 0, 10) = "HTTPClient";
+  if option architecture = 00:0F {
+    filename "<%= @proxy_base_url %>/EFI/grub2/shimia32.efi";
+  } else if option architecture = 00:10 {
+    filename "<%= @proxy_base_url %>/EFI/grub2/shim.efi";
+  }
+  option vendor-class-identifier "HTTPClient"

commit 028f9f7481c6fb748904ffa435e746a60e4f2d5a

Fixes #38259 - Use the correct ssl_port variable
Fixes: f8fb58e2ca06 ("Fixes #30632 - Set up HTTPClients in DHCP")

manifests/proxydhcp.pp
@@ -113,7 +113,7 @@ class foreman_proxy::proxydhcp {
     if $foreman_proxy::http {
       $proxy_base_url = "http://${ip}:${foreman_proxy::http_port}"
     } else {
-      $proxy_base_url = "https://${ip}:${foreman_proxy::https_port}"
+      $proxy_base_url = "https://${ip}:${foreman_proxy::ssl_port}"
     }
 
     dhcp::dhcp_class { 'httpclients':

spec/classes/foreman_proxy__proxydhcp__spec.rb
@@ -237,6 +237,23 @@ describe 'foreman_proxy' do
         end
       end
 
+      context 'with httpboot' do
+        let(:params) { super().merge(httpboot: true, dhcp_pxeserver: '192.0.2.123') }
+
+        context 'with http' do
+          let(:params) { super().merge(http: true) }
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_dhcp__dhcp_class('httpclients').with_parameters(%r{^    filename "http://192\.0\.2\.123:8000/EFI/grub2/shim\.efi";$}) }
+        end
+
+        context 'without http' do
+          let(:params) { super().merge(http: false) }
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_dhcp__dhcp_class('httpclients').with_parameters(%r{^    filename "https://192\.0\.2\.123:8443/EFI/grub2/shim\.efi";$}) }
+        end
+      end
     end
   end
 end